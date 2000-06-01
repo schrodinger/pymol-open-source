4f4d15ecd1442a5e908e91328a0499d8c665680f
wdelano <wdelano@21428953-86e3-433d-9a34-2224224d0fc8>
2000-06-01 14:54:02 +0000

    still working on triangle tesselation...

--- layer0/Triangle.c
+++ layer0/Triangle.c
@@ -17,40 +17,311 @@ Z* -------------------------------------------------------------------
 #include<stdlib.h>
 #include<stdio.h>
 #include<math.h>
+#include<values.h>
 
 #include"Base.h"
 #include"Triangle.h"
 #include"Map.h"
+#include"MemoryDebug.h"
+#include"Err.h"
 
-TriangleSurfaceRec *TrianglePointsToSurface(float *v,int n,float cutoff)
-{
+typedef struct {
+  int index;
+  int value;
+  int next;
+} LinkType;
+
+typedef struct {
+  int *active; /* active edges */
+  int nActive;
+  int *edgeStatus;
+  int *tri;
+  int nTri;
+  float *vBlock; /* direction vector for blocked side of an active edge */
+  int nVBlock;
   MapType *map;
+  LinkType *link;
+  int nLink;
+} TriangleSurfaceRec;
 
-  map=MapNew(cutoff,v,n,NULL);
-  MapSetupExpress(map);
+TriangleSurfaceRec TriangleSurface;
 
-  /*
-  MapLocus(map,v,&h,&k,&l);
-  flag=true;
-  i=*(MapEStart(map,h,k,l));
-  if(i) {
-    j=map->EList[i++];
-    while(j>=0) {
-      if(j!=a) 
-        {
-          a2 = cs->IdxToAtm[j];
-          if(within3f(cs->Coord+3*j,v,cs->Obj->AtomInfo[a2].vdw+probe_radius)) {
-            flag=false;
-            break;
-          }
-        }
-      j=map->EList[i++];
-    }
+static int TriangleEdgeStatus(int i1,int i2) 
+{
+  TriangleSurfaceRec *I=&TriangleSurface;
+  int l,low,high;
+  low = ( i1>i2 ? i2 : i1 );
+  high = ( i1>i2 ? i1 : i2 );
+
+  l=I->edgeStatus[low]; 
+  while(l) {
+	 if(I->link[l].index == high) 
+		return(I->link[l].value); /* -1 closed, 0 open, >0 then has index for blocking vector */
+	 l=I->link[l].next;
   }
-  if(flag)
-    {
-  */
-
-  MapFree(map);
-
+  return(0);
 }
+
+static void TriangleEdgeSetStatus(int i1,int i2,int value) 
+{
+  TriangleSurfaceRec *I=&TriangleSurface;
+  int l,low,high;
+  low = ( i1>i2 ? i2 : i1 );
+  high = ( i1>i2 ? i1 : i2 );
+
+  /*  printf("set: %i %i %i\n",i1,i2,value);*/
+  l=I->edgeStatus[low]; 
+  while(l) {
+	 if(I->link[l].index == high) {
+		I->link[l].value = value; 
+		return;
+	 }
+	 l=I->link[l].next;
+  }
+  if(!l) {
+	 VLACheck(I->link,LinkType,I->nLink);
+	 I->link[I->nLink].next = I->edgeStatus[low];
+	 I->edgeStatus[low] = I->nLink;
+	 /*	 printf("offset %i value %i index %i\n",I->nLink,value,high);*/
+	 I->link[I->nLink].index = high;
+	 I->link[I->nLink].value = value; 
+	 I->nLink++;
+  }
+}
+
+static void TriangleAdd(int i0,int i1,int i2,float *v,float *vn)
+{
+  TriangleSurfaceRec *I=&TriangleSurface;
+  int s01,s12,s02;
+  float *v0,*v1,*v2,*n0,*n1,*n2;
+  float vt[3],vt2[3],nt[3];
+
+  v0 = v+3*i0;
+  v1 = v+3*i1;
+  v2 = v+3*i2;
+  s01 = TriangleEdgeStatus(i0,i1);
+  s02 = TriangleEdgeStatus(i0,i2);
+  s12 = TriangleEdgeStatus(i1,i2);
+  n0 = vn+3*i0;
+  n1 = vn+3*i1;
+  n2 = vn+3*i2;
+  /* create a new triangle */
+  VLACheck(I->tri,int,(I->nTri*3)+2);
+  I->tri[I->nTri*3] = i0;
+  I->tri[I->nTri*3+1] = i1;
+  I->tri[I->nTri*3+2] = i2;
+  I->nTri++;
+  /*				printf("creating %i %i %i\n",i0,i1,i2);*/
+  if(s01) {
+	 TriangleEdgeSetStatus(i0,i1,-1);
+  } else {
+	 VLACheck(I->vBlock,float,(I->nVBlock*3)+2);
+	 subtract3f(v0,v1,nt);
+	 normalize3f(nt);
+	 subtract3f(v2,v1,vt);
+	 remove_component3f(vt,nt,I->vBlock+I->nVBlock*3);
+	 TriangleEdgeSetStatus(i0,i1,I->nVBlock);
+	 I->nVBlock++;
+  }
+  if(s02) {
+	 TriangleEdgeSetStatus(i0,i2,-1);
+  } else {
+	 VLACheck(I->vBlock,float,(I->nVBlock*3)+2);
+	 subtract3f(v0,v2,nt);
+	 normalize3f(nt);
+	 subtract3f(v1,v0,vt);
+	 remove_component3f(vt,nt,I->vBlock+I->nVBlock*3);
+	 TriangleEdgeSetStatus(i0,i2,I->nVBlock);
+	 I->nVBlock++;
+  }
+  if(s12) {
+	 TriangleEdgeSetStatus(i1,i2,-1);
+  } else {
+	 VLACheck(I->vBlock,float,(I->nVBlock*3)+2);
+	 subtract3f(v1,v2,nt);
+	 normalize3f(nt);
+	 subtract3f(v0,v1,vt);
+	 remove_component3f(vt,nt,I->vBlock+I->nVBlock*3);
+	 TriangleEdgeSetStatus(i1,i2,I->nVBlock);
+	 I->nVBlock++;
+  }
+}
+
+int *TrianglePointsToSurface(float *v,float *vn,int n,float cutoff,int *nTriPtr)
+{
+  TriangleSurfaceRec *I=&TriangleSurface;
+  int a;
+  MapType *map;
+  float *v1,*v2,*v0,vt[3],vt2[3],vt3[3],vt4[3],nt[3],*n0,*n1,*n2;
+  int i0,i1,i2,s01,s02,s12,i,j,h,k,l;
+  int doneFlag = false;
+  float dif,minDist,d1,d2,dp;
+  int flag;
+
+  I->nActive = 0;
+  I->active=VLAlloc(int,1000);
+
+  I->link=VLAlloc(LinkType,n*2);
+  I->nLink = 1;
+
+  I->vBlock=VLAlloc(float,n);
+  I->nVBlock = 1;
+
+  I->tri=VLAlloc(int,n);
+  I->nTri = 0;
+
+  I->map=MapNew(cutoff,v,n,NULL);
+  MapSetupExpress(I->map);
+  map=I->map;
+  
+  I->edgeStatus = Alloc(int,n);
+  for(a=0;a<n;a++) {
+	 I->edgeStatus[a]=0;
+  }
+  /* first, let's hit all the easy triangles */
+
+#ifdef _ASDFSD
+
+  for(a=0;a<n;a++) 
+	 {
+		v0=v+a*3;
+		MapLocus(map,v0,&h,&k,&l);
+		i=*(MapEStart(map,h,k,l));
+		if(i) {
+		  j=map->EList[i++];
+		  while(j>=0) {
+			 if(j!=a) 
+				{
+				  dif=diff3f(v+3*j,v0);
+				  if(dif<minDist)
+					 if(TriangleEdgeStatus(a,j)>=0) /* can we put a triangle here? */
+						{
+						  minDist = dif;
+						  i1=a;
+						  i2=j;
+						  doneFlag=false;
+						}
+				}
+			 j=map->EList[i++];
+		  }
+		}
+	 }
+  
+  while(!doneFlag) {
+	 minDist = MAXFLOAT;
+	 doneFlag=true;
+	 for(a=0;a<n;a++) 
+		if(!I->edgeStatus[a])
+		  {
+			 v0=v+a*3;
+			 MapLocus(map,v0,&h,&k,&l);
+			 i=*(MapEStart(map,h,k,l));
+			 if(i) {
+				j=map->EList[i++];
+				while(j>=0) {
+				  if(j!=a) 
+					 {
+						dif=diff3f(v+3*j,v0);
+						if(dif<minDist)
+						  if(TriangleEdgeStatus(a,j)>=0) /* can we put a triangle here? */
+							 {
+								minDist = dif;
+								i1=a;
+								i2=j;
+								doneFlag=false;
+							 }
+					 }
+				  j=map->EList[i++];
+				}
+			 }
+		  }
+	 if(!doneFlag) {
+		VLACheck(I->active,int,I->nActive*2+1);
+		I->active[I->nActive*2] = i1;
+		I->active[I->nActive*2+1] = i2;
+		I->nActive++;
+		while(I->nActive&&(I->nTri<80)) {
+		  I->nActive--;
+		  i1 = I->active[I->nActive*2];
+		  i2 = I->active[I->nActive*2+1];
+		  s12 = TriangleEdgeStatus(i1,i2);
+		  /*		  printf("%i i1:%i i2:%i s12:%i\n",I->nActive,i1,i2,s12);*/
+		  if(s12>=0) {
+
+			 /* find neighboring points */
+			 
+			 minDist = MAXFLOAT;
+			 i0=-1;
+			 v1=v+i1*3;
+			 v2=v+i2*3;
+			 MapLocus(map,v1,&h,&k,&l);
+			 i=*(MapEStart(map,h,k,l));
+			 if(i) {
+				j=map->EList[i++];
+				while(j>=0) {
+				  if((j!=i1)&&(j!=i2))
+					 {
+						v0 = v+3*j;
+						d1 = diff3f(v0,v1);
+						d2 = diff3f(v0,v2);
+						dif= ( d2 > d1 ? d2 : d1 );
+						if(dif<minDist)
+						  {
+							 s01 = TriangleEdgeStatus(j,i1);
+							 s02 = TriangleEdgeStatus(j,i2);
+							 /*							 printf("%i %8.3f %i(%i-%i) %i(%i-%i)\n",j,dif,s01,j,i1,s02,j,i2);*/
+
+							 if((s01>=0)&&(s02>=0))
+								{
+
+								  flag=true;
+
+								  if(s12>0) {
+									 subtract3f(v0,v1,vt);
+									 if(dot_product3f(I->vBlock+s12*3,vt)>0.0)
+										flag=false;
+								  }
+								  if(s01>0) {
+									 subtract3f(v2,v0,vt);
+									 if(dot_product3f(I->vBlock+s01*3,vt)>0.0)
+										flag=false;
+								  }
+								  if(s02>0) {
+									 subtract3f(v1,v0,vt);
+									 if(dot_product3f(I->vBlock+s02*3,vt)>0.0)
+										flag=false;
+								  }
+								  if(flag) {
+									 minDist = dif;
+									 i0=j; 
+								  }
+								}
+						  }
+					 }
+				  j=map->EList[i++];
+				}
+			 }
+			 if(i0<0) {
+				TriangleEdgeSetStatus(i1,i2,-2);				
+			 } else {
+				TriangleAdd(i0,i1,i2,v,vn);
+			 }
+		  }
+		}
+	 }
+	 break;
+  }
+#endif
+
+  printf("NTri: %i\n",I->nTri);
+  (*nTriPtr)=I->nTri;
+  VLAFreeP(I->active);
+  VLAFreeP(I->link);
+  VLAFreeP(I->vBlock);
+  FreeP(I->edgeStatus);
+  MapFree(map);
+  return(I->tri);
+  }
+
+
+
--- layer2/RepSurface.c
+++ layer2/RepSurface.c
@@ -21,23 +21,25 @@ Z* -------------------------------------------------------------------
 #include"OOMac.h"
 #include"RepSurface.h"
 #include"Map.h"
-#include"Isosurf.h"
 #include"Scene.h"
 #include"Sphere.h"
 #include"Setting.h"
 #include"Color.h"
+#include"ObjectMolecule.h"
+#include"Triangle.h"
 
 typedef struct RepSurface {
   Rep R;
   int N;
+  int NT;
   float *V,*VN;
+  int *T;
   int NDot;
   float *Dot;
   int solidFlag;
   Object *Obj;
 } RepSurface;
 
-#include"ObjectMolecule.h"
 
 void RepSurfaceRender(RepSurface *I,CRay *ray,Pickable **pick);
 void RepSurfaceFree(RepSurface *I);
@@ -45,6 +47,8 @@ void RepSurfaceFree(RepSurface *I);
 void RepSurfaceFree(RepSurface *I)
 {
   FreeP(I->V);
+  FreeP(I->VN);
+  VLAFreeP(I->T);
   /*  VLAFreeP(I->N);*/
   OOFreeP(I);
 }
@@ -55,12 +59,31 @@ void RepSurfaceRender(RepSurface *I,CRay *ray,Pickable **pick)
 {
   float *v=I->V;
   float *vn=I->VN;
+  int *t=I->T;
   int c=I->N;
   
   if(ray) {
   } else if(pick) {
   } else {
+	 c=I->NT;
     if(c) {
+      glBegin(GL_TRIANGLES);
+      while(c--)
+        {
+			 
+          glNormal3fv(vn+(*t)*3);
+          glVertex3fv(v+(*t)*3);
+			 t++;
+          glNormal3fv(vn+(*t)*3);
+          glVertex3fv(v+(*t)*3);
+			 t++;
+          glNormal3fv(vn+(*t)*3);
+          glVertex3fv(v+(*t)*3);
+			 t++;
+        }
+		glEnd();
+
+		c=I->N;
       glBegin(GL_POINTS);
       while(c--)
         {
@@ -69,26 +92,27 @@ void RepSurfaceRender(RepSurface *I,CRay *ray,Pickable **pick)
           v+=3;
           vn+=3;
         }
-      glEnd();
+		  glEnd();
     }
   }
 }
 
 #define solv_tole 0.02
+#define minimum_sep 0.5
 
 Rep *RepSurfaceNew(CoordSet *cs)
 {
   ObjectMolecule *obj;
-  int a,b,h,i,j,k,l;
-  MapType *map,*solv_map,*surf_map;
+  int a,b,h,i,j,k,l,c;
+  MapType *map,*solv_map;
   /* grid */
-  float gridSize,v1[3],*v0,*v,*vn;
+  float v1[3],*v0,*v,*vn,*vn0;
   float vdw;
   int SurfaceFlag = false;
   float probe_radius,probe_radius2;
   int inclH = true;
   int cullByFlag = false;
-  int flag;
+  int flag,*dot_flag,*p;
   int ds,a1,a2,c1;
   SphereRec *sp = Sphere0;
   OOAlloc(RepSurface);
@@ -111,10 +135,14 @@ Rep *RepSurfaceNew(CoordSet *cs)
   probe_radius2 = probe_radius*probe_radius;
 
   I->N=0;
+  I->NT=0;
   I->V=NULL;
+  I->VN=NULL;
+  I->T=NULL;
   I->R.fRender=(void (*)(struct Rep *, CRay *, Pickable **))RepSurfaceRender;
   I->R.fFree=(void (*)(struct Rep *))RepSurfaceFree;
   I->Obj = (Object*)(cs->Obj);
+  I->Dot=NULL;
 
   obj = cs->Obj;
 
@@ -128,9 +156,11 @@ Rep *RepSurfaceNew(CoordSet *cs)
   }
   if(SurfaceFlag) {
 
-	 I->V=Alloc(float,cs->NIndex*3*sp->nDot);
+	 I->V=Alloc(float,cs->NIndex*3*sp->nDot*2);
     ErrChkPtr(I->V);
-	 I->VN=Alloc(float,cs->NIndex*3*sp->nDot);
+	 I->VN=Alloc(float,cs->NIndex*3*sp->nDot*2);
+
+
     ErrChkPtr(I->VN);
 	 I->N=0;
     v=I->V;
@@ -216,78 +246,148 @@ Rep *RepSurfaceNew(CoordSet *cs)
         MapFree(solv_map);
         FreeP(I->Dot);	 
         RepSurfaceGetSolventDots(I,cs,probe_radius,Sphere2);
-        solv_map=MapNew(probe_radius,I->Dot,I->NDot,NULL);
-        /* concave surfaces */
-        if(solv_map)
-          {
-            MapSetupExpress(solv_map);
-            for(a=0;a<I->NDot;a++)
-              {
-                v0 = I->Dot+3*a;
-                vdw = probe_radius;
-                for(b=0;b<sp->nDot;b++)
-                  {
-                    v[0]=v0[0]+vdw*sp->dot[b].v[0];
-                    v[1]=v0[1]+vdw*sp->dot[b].v[1];
-                    v[2]=v0[2]+vdw*sp->dot[b].v[2];
-                    MapLocus(solv_map,v,&h,&k,&l);
-                    flag=true;
-                    i=*(MapEStart(solv_map,h,k,l));
-                    if(i) {
-                      j=solv_map->EList[i++];
-                      while(j>=0) {
-                        if(j!=a) 
-                          {
-                            if(within3f(I->Dot+3*j,v,probe_radius)) {
-                              flag=false;
-                              break;
-                            }
-                          }
-                        j=solv_map->EList[i++];
-                      }
-                    }
-                    if(flag)
-                      {
-                        MapLocus(map,v,&h,&k,&l);
-                        flag=true;
-                        i=*(MapEStart(map,h,k,l));
-                        if(i) {
-                          j=map->EList[i++];
-                          while(j>=0) {
-                            a2 = cs->IdxToAtm[j];
-                            if((inclH||(obj->AtomInfo[a2].name[0]!='H'))&&
-                               ((!cullByFlag)||(!(obj->AtomInfo[a2].customFlag&0x2))))  
-                              /* ignore if the "2" bit is set */
-                              if(j!=a)
-                                if(within3f(cs->Coord+3*j,v,
-                                            cs->Obj->AtomInfo[a2].vdw+probe_radius+solv_tole))
-                                  {
-                                    flag=false;
-                                    break;
-                                  }
-                            j=map->EList[i++];
-                          }
-                        }
-                        if(!flag) {
-                          I->N++;
-                          v+=3;
-                          *(vn++)=-sp->dot[b].v[0];
-                          *(vn++)=-sp->dot[b].v[1];
-                          *(vn++)=-sp->dot[b].v[2];
-                        }
-                      }
-                  }
-              }
-            MapFree(solv_map);
-          }
-        MapFree(map);
-      }
-    FreeP(I->Dot);	 
-	 I->V = Realloc(I->V,float,(v-I->V));
-	 I->VN = Realloc(I->VN,float,(vn-I->VN));
+		  if(I->NDot) {
+			 solv_map=MapNew(probe_radius,I->Dot,I->NDot,NULL);
+			 /* concave surfaces */
+			 if(solv_map)
+				{
+				  MapSetupExpress(solv_map);
+				  for(a=0;a<I->NDot;a++)
+					 {
+						v0 = I->Dot+3*a;
+						vdw = probe_radius;
+						for(b=0;b<sp->nDot;b++)
+						  {
+							 v[0]=v0[0]+vdw*sp->dot[b].v[0];
+							 v[1]=v0[1]+vdw*sp->dot[b].v[1];
+							 v[2]=v0[2]+vdw*sp->dot[b].v[2];
+							 MapLocus(solv_map,v,&h,&k,&l);
+							 flag=true;
+							 i=*(MapEStart(solv_map,h,k,l));
+							 if(i) {
+								j=solv_map->EList[i++];
+								while(j>=0) {
+								  if(j!=a) 
+									 {
+										if(within3f(I->Dot+3*j,v,probe_radius)) {
+										  flag=false;
+										  break;
+										}
+									 }
+								  j=solv_map->EList[i++];
+								}
+							 }
+							 if(flag)
+								{
+								  MapLocus(map,v,&h,&k,&l);
+								  flag=true;
+								  i=*(MapEStart(map,h,k,l));
+								  if(i) {
+									 j=map->EList[i++];
+									 while(j>=0) {
+										a2 = cs->IdxToAtm[j];
+										if((inclH||(obj->AtomInfo[a2].name[0]!='H'))&&
+											((!cullByFlag)||(!(obj->AtomInfo[a2].customFlag&0x2))))  
+										  /* ignore if the "2" bit is set */
+										  if(j!=a)
+											 if(within3f(cs->Coord+3*j,v,
+															 cs->Obj->AtomInfo[a2].vdw+probe_radius+solv_tole))
+												{
+												  flag=false;
+												  break;
+												}
+										j=map->EList[i++];
+									 }
+								  }
+								  if(!flag) {
+									 I->N++;
+									 v+=3;
+									 *(vn++)=-sp->dot[b].v[0];
+									 *(vn++)=-sp->dot[b].v[1];
+									 *(vn++)=-sp->dot[b].v[2];
+								  }
+								}
+						  }
+					 }
+				  MapFree(solv_map);
+				}
+			 MapFree(map);
+		  }
+		  printf("%i\n",I->N);
+		  FreeP(I->Dot);	 
+		}
+	 /* now, eliminate dots that are too close to each other*/
+	 if(I->N) 
+		{
+		  dot_flag=Alloc(int,I->N);
+		  ErrChkPtr(dot_flag);
+		  for(a=0;a<I->N;a++) {
+			 dot_flag[a]=1;
+		  }
+		  
+		  map=MapNew(minimum_sep,I->V,I->N,NULL);
+		  if(map)
+			 {
+				MapSetupExpress(map);		  
+				v=I->V;
+				for(a=0;a<I->N;a++) {
+				  if(dot_flag[a]) {
+					 MapLocus(map,v,&h,&k,&l);
+					 i=*(MapEStart(map,h,k,l));
+					 if(i) {
+						j=map->EList[i++];
+						while(j>=0) {
+						  if(j!=a) 
+							 {
+								if(dot_flag[j]) {
+								  if(within3f(I->V+(3*j),v,minimum_sep)) {
+									 dot_flag[j]=0;
+									 break;
+								  }
+								}
+							 }
+						  j=map->EList[i++];
+						}
+					 }
+				  }
+				  v+=3;
+				}
+				MapFree(map);
+			 }
+		  v0=I->V;
+		  v=I->V;
+		  vn0=I->VN;
+		  vn=I->VN;
+		  p=dot_flag;
+		  c=I->N;
+		  I->N=0;
+		  for(a=0;a<c;a++)
+			 {
+				if(*(p++)) {
+				  *(v0++)=*(v++);
+				  *(v0++)=*(v++);
+				  *(v0++)=*(v++);
+				  *(vn0++)=*(vn++);
+				  *(vn0++)=*(vn++);
+				  *(vn0++)=*(vn++);
+				  I->N++;
+				} else {
+				  v+=3;
+				  vn+=3;
+				}
+			 }
+		  FreeP(dot_flag);
+		}
+	 if(I->N) {	
+		I->V = Realloc(I->V,float,(v0-I->V));
+		I->VN = Realloc(I->VN,float,(vn0-I->VN));
+		I->T=TrianglePointsToSurface(I->V,I->VN,I->N,probe_radius,&I->NT);
 
-    /* tesselation */
-    
+	 } else {
+		I->V = Realloc(I->V,float,1);
+		I->VN = Realloc(I->VN,float,1);
+	 }
+
   }
   OrthoBusyFast(4,4);
   return((void*)(struct Rep*)I);
