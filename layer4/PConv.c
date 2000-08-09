4e75d5d1bb6b7ae60d8968f779149ec673730b23
wdelano <wdelano@21428953-86e3-433d-9a34-2224224d0fc8>
2000-08-09 10:16:28 +0000

    Reworking the thread handling code to enable safe entry of non-master threads into the PyMOL API and safe execution of Python code including re-entrant code from within the API.

---
+++ layer4/PConv.c
@@ -0,0 +1,38 @@
+
+/* 
+A* -------------------------------------------------------------------
+B* This file contains source code for the PyMOL computer program
+C* copyright 1998-2000 by Warren Lyford Delano of DeLano Scientific. 
+D* -------------------------------------------------------------------
+E* It is unlawful to modify or remove this copyright notice.
+F* -------------------------------------------------------------------
+G* Please see the accompanying LICENSE file for further information. 
+H* -------------------------------------------------------------------
+I* Additional authors of this source file include:
+-* 
+-* 
+-*
+Z* -------------------------------------------------------------------
+*/
+
+#include<stdlib.h>
+#include<Python.h>
+#include<signal.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/time.h>
+#include<unistd.h>
+
+#include"MemoryDebug.h"
+#include"Base.h"
+#include"PConv.h"
+#include"PUtils.h"
+
+extern PyThreadState *_save;
+
+void PConv44To44f(PyObject *src,float *dest)
+{
+  PBlock();
+
+  PUnblock();
+}
---
+++ modules/pmx.py
@@ -0,0 +1,45 @@
+#A* -------------------------------------------------------------------
+#B* This file contains source code for the PyMOL computer program
+#C* copyright 1998-2000 by Warren Lyford Delano of DeLano Scientific. 
+#D* -------------------------------------------------------------------
+#E* It is unlawful to modify or remove this copyright notice.
+#F* -------------------------------------------------------------------
+#G* Please see the accompanying LICENSE file for further information. 
+#H* -------------------------------------------------------------------
+#I* Additional authors of this source file include:
+#-* 
+#-* 
+#-*
+#Z* -------------------------------------------------------------------
+
+import sglite
+
+# pmx.py 
+# This section contains python code for supporting
+# x-ray crystallography functions
+
+def sg_sym_to_mat_list(sgsymbol):
+   try:
+      Symbols_Inp = sglite.SgSymbolLookup(sgsymbol)
+      if(Symbols_Inp):
+         HallSymbol = Symbols_Inp['Hall']
+         SgOps = sglite.SgOps(HallSymbol)
+         nLTr = SgOps.get_nLTr()
+         fInv = SgOps.get_fInv()
+         nSMx = SgOps.get_nSMx()
+         result = []
+         rb = float(sglite.SRBF)
+         tb = float(sglite.STBF)
+         for iLTr in xrange(nLTr):
+            for iInv in xrange(fInv):
+               for iSMx in xrange(nSMx):
+                  Mx = SgOps.getLISMx(iLTr, iInv, iSMx, +1)
+                  result.append([[ Mx[0]/rb, Mx[1]/rb, Mx[2]/rb, Mx[9 ]/tb],
+                                 [ Mx[3]/rb, Mx[4]/rb, Mx[5]/rb, Mx[10]/tb],
+                                 [ Mx[6]/rb, Mx[7]/rb, Mx[8]/rb, Mx[11]/tb],
+                                 [        0,        0,        0,         1]] )                  
+   except:
+      print " pmx: unrecognized space group symbol '"+sgsymbol+"'"
+      result = None
+   return result
+
