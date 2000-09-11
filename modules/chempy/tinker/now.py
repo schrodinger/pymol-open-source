6b3228a7fa9b9da33f6fd9f36d5368ab46369295
wdelano <wdelano@21428953-86e3-433d-9a34-2224224d0fc8>
2000-09-11 09:20:43 +0000

    Just some proof of concept stuff...

--- modules/chempy/tinker/now.py
+++ modules/chempy/tinker/now.py
@@ -33,6 +33,21 @@ def minimize(model,gradient=5.0,maxiter=None,kw=None):
    tinker.run('minimize',inp_prefix,out_prefix,[tinker.prefix,str(gradient)])
    io.xyz.updateFromFile(model,out_prefix+".xyz")
 
+def dynamics(model,steps=100,timestep=1.0,dumps=0.1,temperature=300,kw=None):
+   io.xyz.toFile(model,inp_prefix+".xyz")
+   if not kw:
+      kw = []
+      kw.extend(def_keywords)
+   kw.append("overwrite\n")
+   kw.extend(keyword.get_partial_charge(model))   
+   _write_keys(kw)
+   tinker.run('dynamic',inp_prefix,out_prefix,
+              [tinker.prefix,str(steps),
+               str(timestep),str(dumps),
+               str(temperature)])
+   io.xyz.updateFromFile(model,out_prefix+".001")
+
+
 def analyze(model,gradient=5.0,maxiter=None,kw=None):
    io.xyz.toFile(model,inp_prefix+".xyz")
    if not kw:
