6307c7c253c3929b9c0b7789d3be590cc3acdafb
wdelano <wdelano@21428953-86e3-433d-9a34-2224224d0fc8>
2000-09-11 17:52:04 +0000

    cPickle works.

--- trunk/pymol/modules/chempy/pkl.py
+++ trunk/pymol/modules/chempy/pkl.py
@@ -1,27 +1,27 @@
 
 from chempy import Storage
 
-import pickle
+import cPickle
 
 class PKL(Storage):
 
    def fromFile(self,fname,**params):
       fp = open(fname)
-      result = load(fp)
+      result = cPickle.load(fp)
       fp.close()
       return result
 
 #---------------------------------------------------------------------------
    def toFile(self,indexed,fname,**params):
       fp = open(fname,'w')
-      result = dump(indexed,fp)
+      result = cPickle.dump(indexed,fp,1)
       fp.close()
       
 #---------------------------------------------------------------------------
    def fromString(self,st):   
-      return loads(st)
+      return cPickle.loads(st)
 
 #---------------------------------------------------------------------------
    def toString(self,model):
-      return dumps(st)
+      return cPickle.dumps(st)
 
