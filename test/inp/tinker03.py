62924bb63eb87f1516453a0f836a9bff78fd146e
wdelano <wdelano@21428953-86e3-433d-9a34-2224224d0fc8>
2000-09-13 03:05:39 +0000

    Chempy/Tinker tests.

---
+++ test/inp/tinker03.py
@@ -0,0 +1,40 @@
+# python
+
+# dynamics test
+
+from chempy import io
+from chempy import tinker
+from chempy import protein
+from chempy.tinker.state import State
+from chempy import feedback
+
+import os
+
+feedback['atoms'] = 1
+
+m = io.pdb.fromFile("dat/pept.pdb")
+
+m = protein.generate(m)
+
+s = State()
+
+s.echo = 0
+
+s.load_model(m)
+print " test: atom 0 position:",m.atom[0].coord
+s.dynamics(steps=50,interval=10)
+
+print " test:",len(s.frames),"frames returned."
+print " test: atom 0 position:",m.atom[0].coord
+
+s.dynamics(steps=50,interval=10)
+print " test: atom 0 position:",m.atom[0].coord
+
+print " test: energy is ->",s.energy
+
+for a in s.summary:
+   print " test: summary ",a
+
+os.system("touch .no_fail tinker_*")
+os.system("/bin/rm .no_fail tinker_*")
+
