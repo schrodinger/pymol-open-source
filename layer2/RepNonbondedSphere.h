9cae5805aa066fc664245048c7463c521f75df4d
wdelano <wdelano@21428953-86e3-433d-9a34-2224224d0fc8>
2000-09-17 01:12:50 +0000

    Forgotten headers...

---
+++ layer2/RepNonbondedSphere.h
@@ -0,0 +1,25 @@
+/* 
+A* -------------------------------------------------------------------
+B* This file contains source code for the PyMOL computer program
+C* copyright 1998-2000 by Warren Lyford Delano of DeLano Scientific. 
+D* -------------------------------------------------------------------
+E* It is unlawful to modify or remove this copyright notice.
+F* -------------------------------------------------------------------
+G* Please see the accompanying LICENSE file for further information. 
+H* -------------------------------------------------------------------
+I* Additional authors of this source file include:
+-* 
+-* 
+-*
+Z* -------------------------------------------------------------------
+*/
+#ifndef _H_RepNonbondedSphere
+#define _H_RepNonbondedSphere
+
+#include"Rep.h"
+#include"CoordSet.h"
+
+Rep *RepNonbondedSphereNew(CoordSet *cset);
+void RepNonbondedSphereInit(void);
+
+#endif
