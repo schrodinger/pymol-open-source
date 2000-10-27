39b33e1c8909e2013d6cd213ff6fa2ff43580c4c
wdelano <wdelano@21428953-86e3-433d-9a34-2224224d0fc8>
2000-10-27 18:27:44 +0000

    Generalized parameter set for both proteins and small molecules.

---
+++ trunk/pymol/modules/chempy/fragments/__init__.py
@@ -0,0 +1,9 @@
+
+import chempy
+from chempy import io
+
+path = chempy.path + 'fragments/'
+
+def get(name):
+   return io.pkl.fromFile(path+name+'.pkl')
+
---
+++ trunk/pymol/modules/chempy/tinker/make99_simple.py
@@ -0,0 +1,101 @@
+import string
+
+f = open("parm99_wld.dat")
+g = open("parm_simple.dat")
+h = open("parm99_simple.dat",'w')
+
+while 1:
+   l = f.readline()
+   if not len(string.strip(l)):
+      break
+   h.write(l)
+
+g.readline() # skip first line
+while 1:
+   l = g.readline()
+   h.write(l)
+   if not len(string.strip(l)):
+      break
+   
+l=f.readline()
+h.write(l)
+g.readline() # skip this stuff for now
+
+while 1:
+   l = f.readline()
+   if not len(string.strip(l)):
+      break
+   h.write(l)
+
+while 1:
+   l = g.readline()
+   h.write(l)
+   if not len(string.strip(l)):
+      break
+
+while 1:
+   l = f.readline()
+   if not len(string.strip(l)):
+      break
+   h.write(l)
+
+while 1:
+   l = g.readline()
+   h.write(l)
+   if not len(string.strip(l)):
+      break
+
+while 1:
+   l = f.readline()
+   if not len(string.strip(l)):
+      break
+   h.write(l)
+
+while 1:
+   l = g.readline()
+   h.write(l)
+   if not len(string.strip(l)):
+      break
+
+while 1:
+   l = f.readline()
+   h.write(l)
+   if l[0:4]=='MOD4':
+      break
+
+while 1:
+   l = g.readline()
+   if l[0:4]=='MOD4':
+      break
+
+while 1:
+   l = f.readline()
+   if not len(string.strip(l)):
+      break
+   h.write(l)
+
+while 1:
+   l = g.readline()
+   h.write(l)
+   if not len(string.strip(l)):
+      break
+
+h.write(f.readline())
+g.readline()
+h.write(f.readline())
+g.readline()
+h.write(f.readline())
+g.readline()
+
+while 1:
+   l = f.readline()
+   if not l: break
+   if l[0:6]=='TINKER':
+      h.write(l)
+
+while 1:
+   l = g.readline()
+   if not l: break
+   if l[0:6]=='TINKER':
+      h.write(l)
+
