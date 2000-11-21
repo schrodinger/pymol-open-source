8741b5f79fa7ab56a40ca5d0f4ae66475ef9a07c
wdelano <wdelano@21428953-86e3-433d-9a34-2224224d0fc8>
2000-11-21 19:26:35 +0000

    list object

--- trunk/pymol/modules/chempy/io.py
+++ trunk/pymol/modules/chempy/io.py
@@ -1,18 +1,23 @@
 
 # Input/output facility
 
+from chempy.pkl import PKL
+from chempy.lst import LST
 from chempy.pdb import PDB
 from chempy.xyz import XYZ
 from chempy.mol import MOL
-from chempy.pkl import PKL
 from chempy.arc import ARC
 from chempy.gms import GMS
 from chempy.mmd import MMD
 
+pkl = PKL() # general object io
+lst = LST() # general string-list io
+
+# specific for indexed model objects:
+
 pdb = PDB()
 xyz = XYZ()
 mol = MOL()
-pkl = PKL()
 arc = ARC()
 gms = GMS() # OBSOLETE - PLEASE DO NOT USE
 mmd = MMD()
