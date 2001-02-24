2842aca7cb3fd2756fb7e0f57430a1ea385498eb
wdelano <wdelano@21428953-86e3-433d-9a34-2224224d0fc8>
2001-02-24 12:05:48 +0000

    pyopengl

---
+++ contrib/pyopengl/_opengl_nummodule.c
@@ -0,0 +1,17 @@
+#ifdef _PYMOL_NUMPY
+
+#define NUMERIC
+#include "_openglmodule.c"
+
+#else
+
+#ifdef WIN32
+#include<windows.h>
+#endif
+
+#include<Python.h>
+
+DL_EXPORT(void)
+init_opengl_num(void) {};
+
+#endif
