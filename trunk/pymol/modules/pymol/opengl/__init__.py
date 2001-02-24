cdb7eb19611fd0d68842d758a9196f058961a941
wdelano <wdelano@21428953-86e3-433d-9a34-2224224d0fc8>
2001-02-24 12:11:45 +0000

    pyopengl

---
+++ trunk/pymol/modules/pymol/opengl/__init__.py
@@ -0,0 +1,7 @@
+try:
+    import multiarray
+    _numeric = 1
+except ImportError:
+    _numeric = 0
+
+__version__ = "1.5.6b1"
---
+++ trunk/pymol/modules/pymol/opengl/glu/__init__.py
@@ -0,0 +1,39 @@
+# $Id$
+import sys
+from pymol import opengl
+
+if opengl._numeric:
+    from Numeric import ArrayType
+    try:
+        import _glu_num
+        _glu = _glu_num
+    except ImportError:
+        import _glu
+    except SystemError:
+        import _glu
+else:
+    import _glu
+
+from gluconst import *
+from pymol.opengl.gl import Error, CarefulFunction
+
+origdict = _glu.__dict__.copy()
+sys.modules['pymol.opengl.glu'].__dict__.update(origdict)
+
+carefuldict = {}
+for name, func in origdict.items():
+    if callable(func):
+        carefuldict[name] = CarefulFunction(name, func)
+# These do the same sorts of things that the C versions would
+
+def careful():
+    cd = carefuldict.copy()
+    if cd.has_key('error'): del cd['error']
+    sys.modules['pymol.opengl.glu'].__dict__.update(cd)
+
+import string
+def fast():
+    cd = origdict.copy()
+    if cd.has_key('error'): del cd['error']
+    if cd.has_key('glconst'): del cd['glconst']
+    sys.modules['pymol.opengl.glu'].__dict__.update(cd)
