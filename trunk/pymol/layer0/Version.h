1aa65454d8bc925cff5e9c52ddd53722481f61d7
wdelano <wdelano@21428953-86e3-433d-9a34-2224224d0fc8>
2001-03-07 08:33:36 +0000

    bugs

--- trunk/pymol/layer0/Version.h
+++ trunk/pymol/layer0/Version.h
@@ -1,5 +1,5 @@
 #ifndef _PyMOL_VERSION
-#define _PyMOL_VERSION "0.45"
+#define _PyMOL_VERSION "0.46"
 #endif
 
 
--- trunk/pymol/layer1/PConv.c
+++ trunk/pymol/layer1/PConv.c
@@ -146,17 +146,19 @@ int PConvPyObjectToStrMaxLen(PyObject *object,char *value,int ln)
   else if(PyString_Check(object)) {
     st = PyString_AsString(object);
     strncpy(value,st,ln);
-    value[ln]=0;
   } else {
     tmp = PyObject_Str(object);
     if(tmp) {
       st = PyString_AsString(tmp);
       strncpy(value,st,ln);
-      value[ln]=0;
       Py_DECREF(tmp);
     } else
       result=0;
   }
+  if(ln>0)
+    value[ln-1]=0;
+  else
+    value[0]=0;
   return(result);
 }
 
@@ -170,17 +172,19 @@ int PConvPyObjectToStrMaxClean(PyObject *object,char *value,int ln)
   else if(PyString_Check(object)) {
     st = PyString_AsString(object);
     strncpy(value,st,ln);
-    value[ln]=0;
   } else {
     tmp = PyObject_Str(object);
     if(tmp) {
       st = PyString_AsString(tmp);
       strncpy(value,st,ln);
-      value[ln]=0;
       Py_DECREF(tmp);
     } else
       result=0;
   }
+  if(ln>0)
+    value[ln-1]=0;
+  else
+    value[0]=0;
   UtilCleanStr(value);
   return(result);
 }
