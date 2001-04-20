788f9d0fb9ceca6156f54234f4bee8a7b7de836f
wdelano <wdelano@21428953-86e3-433d-9a34-2224224d0fc8>
2001-04-20 21:56:01 +0000

    example

---
+++ trunk/pymol/examples/chempy/generate_amber.py
@@ -0,0 +1,20 @@
+# python
+
+from chempy import io
+from chempy import protein
+from chempy import protein_amber99
+
+
+model= io.pdb.fromFile("../../test/dat/pept.pdb")
+
+model= protein.generate(model,forcefield=protein_amber99)
+
+sm = 0
+for a in model.atom:
+   sm = sm + a.partial_charge
+
+print " prot: net partial charge on protein is %8.4f" % sm
+print " prot: (this should be integral)!"
+
+io.pkl.toFile(model,"generate_amber.pkl")
+
