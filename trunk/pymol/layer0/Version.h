d8c4c0b84ba0340e9de470cdc6e8becb1f1ac7d9
wdelano <wdelano@21428953-86e3-433d-9a34-2224224d0fc8>
2001-10-16 10:29:01 +0000

    *** empty log message ***

--- trunk/pymol/layer0/Version.h
+++ trunk/pymol/layer0/Version.h
@@ -1,9 +1,9 @@
 #ifndef _PyMOL_VERSION
-#define _PyMOL_VERSION "0.66"
+#define _PyMOL_VERSION "0.67"
 #endif
 
 #ifndef _PyMOL_VERSION_int 
-#define _PyMOL_VERSION_int 66
+#define _PyMOL_VERSION_int 67
 #endif
 
 
