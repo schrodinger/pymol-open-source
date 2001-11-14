29878936754884ed8f057ceb8684ce548279841f
wdelano <wdelano@21428953-86e3-433d-9a34-2224224d0fc8>
2001-11-14 03:24:43 +0000

    *** empty log message ***

---
+++ trunk/pymol/modules/pmg_tk/startup/__init__.py
@@ -0,0 +1,21 @@
+# add your pmg_tk startup scripts to this directory...
+# here is an example: (copy, remove comment marks, and save as sample.py)
+
+# BEGIN EXAMPLE: sample.py
+
+#from Tkinter import *
+#from pymol import cmd
+
+#def __init__(self):
+#   self.menuBar.addmenu('Sample', 'Sample Menu')
+
+#   self.menuBar.addmenuitem('Sample', 'command',
+#                      'White background',
+#                      label='White Background',
+#                     command = lambda : cmd.set("bg_rgb","[1,1,1]"))
+
+#   self.menuBar.addmenuitem('Sample', 'command',
+#                      'Black background',
+#                      label='Black Background',
+#                     command = lambda : cmd.set("bg_rgb","[0,0,0]"))
+
