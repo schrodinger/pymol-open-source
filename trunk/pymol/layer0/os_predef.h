091982443749f06c16690aacceeb86208c71747f
wdelano <wdelano@21428953-86e3-433d-9a34-2224224d0fc8>
2002-02-09 05:26:12 +0000

    *** empty log message ***

--- trunk/pymol/layer0/os_predef.h
+++ trunk/pymol/layer0/os_predef.h
@@ -16,6 +16,86 @@ Z* -------------------------------------------------------------------
 #ifndef _H_os_predef
 #define _H_os_predef
 
+#ifdef _WIN32
+#ifndef _AFXDLL
+	#ifndef _UNICODE
+		#ifdef _DEBUG
+			#pragma comment(lib, "nafxcwd.lib")
+		#else
+			#pragma comment(lib, "nafxcw.lib")
+		#endif
+	#else
+		#ifdef _DEBUG
+			#pragma comment(lib, "uafxcwd.lib")
+		#else
+			#pragma comment(lib, "uafxcw.lib")
+		#endif
+	#endif
+#else
+	#ifndef _UNICODE
+		#ifdef _DEBUG
+			#pragma comment(lib, "mfc42d.lib")
+			#pragma comment(lib, "mfcs42d.lib")
+		#else
+			#pragma comment(lib, "mfc42.lib")
+			#pragma comment(lib, "mfcs42.lib")
+		#endif
+	#else
+		#ifdef _DEBUG
+			#pragma comment(lib, "mfc42ud.lib")
+			#pragma comment(lib, "mfcs42ud.lib")
+		#else
+			#pragma comment(lib, "mfc42u.lib")
+			#pragma comment(lib, "mfcs42u.lib")
+		#endif
+	#endif
+#endif
+
+#ifdef _DLL
+	#if !defined(_AFX_NO_DEBUG_CRT) && defined(_DEBUG)
+		#pragma comment(lib, "msvcrtd.lib")
+	#else
+		#pragma comment(lib, "msvcrt.lib")
+	#endif
+#else
+#ifdef _MT
+	#if !defined(_AFX_NO_DEBUG_CRT) && defined(_DEBUG)
+		#pragma comment(lib, "libcmtd.lib")
+	#else
+		#pragma comment(lib, "libcmt.lib")
+	#endif
+#else
+	#if !defined(_AFX_NO_DEBUG_CRT) && defined(_DEBUG)
+		#pragma comment(lib, "libcd.lib")
+	#else
+		#pragma comment(lib, "libc.lib")
+	#endif
+#endif
+#endif
+
+#pragma comment(lib, "kernel32.lib")
+#pragma comment(lib, "user32.lib")
+#pragma comment(lib, "gdi32.lib")
+#pragma comment(lib, "comdlg32.lib")
+#pragma comment(lib, "winspool.lib")
+#pragma comment(lib, "advapi32.lib")
+#pragma comment(lib, "shell32.lib")
+#pragma comment(lib, "comctl32.lib")
+
+// force inclusion of NOLIB.OBJ for /disallowlib directives
+#pragma comment(linker, "/include:__afxForceEXCLUDE")
+
+// force inclusion of DLLMODUL.OBJ for _USRDLL
+#ifdef _USRDLL
+#pragma comment(linker, "/include:__afxForceUSRDLL")
+#endif
+
+// force inclusion of STDAFX.OBJ for precompiled types
+#ifdef _AFXDLL
+#pragma comment(linker, "/include:__afxForceSTDAFX")
+#endif
+
+#endf
 
 
 #endif
