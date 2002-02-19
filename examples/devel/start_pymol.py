e9036d0230a2d47c91eeef560d57173e91e66794
wdelano <wdelano@21428953-86e3-433d-9a34-2224224d0fc8>
2002-02-19 20:14:53 +0000

    *** empty log message ***

--- examples/devel/start_pymol.py
+++ examples/devel/start_pymol.py
@@ -1,32 +1,35 @@
 # ================================================================
-# NOTE: THIS APPROACH IS NOT RECOMMENDED, BUT THE PyMOL LAUNCH
-#       PROCESS IS SUBJECT TO CHANGE
+# NOTE: THIS APPROACH IS NOT RECOMMENDED SINCE THE PyMOL LAUNCH
+#       PROCESS IS SUBJECT TO CHANGE WITHOUT WARNING
 #
-# The recommended approach is to just use:
+# The recommended approach is to just use PyMOL as your python
+# interpreter:
+#
 #     "pymol -r <script.py>" or
-#     "pymol -q -c -r <script.py>" instead of
+#     "pymol -qcr <script.py>" instead of
 #     "python <script.py>"
 # 
-# However, If must invoke PyMOL from an external interpreter,
-# you need to:
+# However, If must invoke PyMOL from an external interpreter, then
+# you willl need to:
 #
-#  (1) prepare the environment by sourcing "pymol.csh"
-#      or an equivalent (to define PYMOL_PATH,
-#      LD_LIBRARY_PATH, TCL_LIBRARY, etc.)
+#  (1) prepare the environment first by sourcing "pymol.csh"
+#      or an equivalent shell script which defines PYMOL_PATH,
+#      LD_LIBRARY_PATH, TCL_LIBRARY, etc.
 #
-#  (2) include the following code at the *top* level of your
+#  (2) include the following code block at the *top* level of your
 #      program script.
 #
-# === Define any argument to PyMOL you might want to indicate
-pymol_argv = [ "pymol", "-c", "-q" ]
-# (Note: first argument must be "pymol")
+# ================================================================
+# PyMOL launch code
 #
-# === Launch the PyMOL thread
+# === Provide arguments to PyMOL (first one must be "pymol")
+pymol_argv = [ "pymol", "-q" ]
+#
+# === Launch the PyMOL thread(s)
 import os,threading,__main__,__builtin__
-t = threading.Thread(target=__builtin__.execfile,
-    args=(os.environ['PYMOL_PATH']+"/modules/launch_pymol.py",
-          __main__.__dict__,__main__.__dict__))
-t.start()
+threading.Thread(target=__builtin__.execfile,
+     args=(os.environ['PYMOL_PATH']+"/modules/launch_pymol.py",
+           __main__.__dict__,__main__.__dict__)).start()
 #
 # === Wait until PyMOL is ready to receive commands
 e=threading.Event()
@@ -34,12 +37,13 @@ while not hasattr(__main__,'pymol'):
    e.wait(0.01)
 while not pymol._cmd.ready():
    e.wait(0.01)
-# You can now safely make use of pymol
+#
+# PyMOL is now launched, you can now import "pymol" modules.
 # ===============================================================
 
-#from pymol import cmd
+from pymol import cmd
 
-#cmd.load("$PYMOL_PATH/test/dat/pept.pdb")
-#print " The surface area is: %8.3f"%cmd.get_area()
+cmd.load("$PYMOL_PATH/test/dat/pept.pdb")
+print " The surface area is: %8.3f"%cmd.get_area()
 
 
