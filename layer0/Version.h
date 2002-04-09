be7be2d66b3f5c6972b02325b6befa02cf9ef9cf
wdelano <wdelano@21428953-86e3-433d-9a34-2224224d0fc8>
2002-04-09 06:47:27 +0000

    *** empty log message ***

--- layer0/Version.h
+++ layer0/Version.h
@@ -1,9 +1,9 @@
 #ifndef _PyMOL_VERSION
-#define _PyMOL_VERSION "0.79"
+#define _PyMOL_VERSION "0.80"
 #endif
 
 #ifndef _PyMOL_VERSION_int 
-#define _PyMOL_VERSION_int 79
+#define _PyMOL_VERSION_int 80
 #endif
 
 
