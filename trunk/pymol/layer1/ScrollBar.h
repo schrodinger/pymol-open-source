b18d7603a5752a535c36a5bd5684adc5d8933c77
wdelano <wdelano@21428953-86e3-433d-9a34-2224224d0fc8>
2002-10-09 05:30:36 +0000

    *** empty log message ***

---
+++ trunk/pymol/layer1/ScrollBar.h
@@ -0,0 +1,35 @@
+/* 
+A* -------------------------------------------------------------------
+B* This file contains source code for the PyMOL computer program
+C* copyright 1998-2000 by Warren Lyford Delano of DeLano Scientific. 
+D* -------------------------------------------------------------------
+E* It is unlawful to modify or remove this copyright notice.
+F* -------------------------------------------------------------------
+G* Please see the accompanying LICENSE file for further information. 
+H* -------------------------------------------------------------------
+I* Additional authors of this source file include:
+-* 
+-* 
+-*
+Z* -------------------------------------------------------------------
+*/
+#ifndef _H_ScrollBar
+#define _H_ScrollBar
+
+struct CScrollBar;
+
+struct CScrollBar *ScrollBarNew(int horizontal);
+void ScrollBarFree(struct CScrollBar *I);
+void ScrollBarSetBox(struct CScrollBar *I,int top,int left,int bottom, int right);
+void ScrollBarDoDraw(struct CScrollBar *I);
+void ScrollBarSetLimits(struct CScrollBar *I,int list_size,int display_size);
+void ScrollBarSetValue(struct CScrollBar *I,int value);
+void ScrollBarDoRelease(struct CScrollBar *I,int button,int x,int y,int mod);
+void ScrollBarDoClick(struct CScrollBar *I,int button,int x,int y,int mod);
+void ScrollBarDoDrag(struct CScrollBar *I,int x,int y,int mod);
+Block *ScrollBarGetBlock(struct CScrollBar *);
+int ScrollBarGetValue(struct CScrollBar *I);
+void ScrollBarUpdate(struct CScrollBar *I);
+
+#endif
+
