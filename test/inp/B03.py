2954f317ed1e7e81c081db14fb2d9ebd4caf4ecb
wdelano <wdelano@21428953-86e3-433d-9a34-2224224d0fc8>
2003-04-27 22:25:58 +0000

    *** empty log message ***

--- test/inp/B03.py
+++ test/inp/B03.py
@@ -3,7 +3,7 @@
 #
       
 from glob import glob
-
+import traceback
 import threading
 import time
 import whrandom
@@ -13,21 +13,29 @@ import sys, os, os.path
 ent_dir = "pdb/*"
 
 def load():
-   r = 0
-   reps = [ "lines", "sticks", "cartoon" ]
-   list = glob(ent_dir)
-   for dir in list:
-      print dir
-      cmd.delete("all")
-      for file in glob(dir+"/pdb*"):
-         cmd.load(file)
-         cmd.refresh()
-         time.sleep(0.5)
-      cmd.zoom()
-      for rep in reps:
-         cmd.show(rep)
-         cmd.refresh()
-         time.sleep(0.5)         
+   try:
+      cmd.mplay()
+      r = 0
+      reps = [ "lines", "sticks", "cartoon" ]
+      list = glob(ent_dir)
+      for dir in list:
+         print dir
+         cmd.delete("all")
+         for file in glob(dir+"/pdb*"):
+            cmd.load(file)
+            cmd.refresh()
+            cmd.sync()
+            time.sleep(0.1)
+         cmd.zoom()
+         for rep in reps:
+            cmd.show(rep)
+            cmd.refresh()
+            cmd.sync()
+            time.sleep(0.1)
+         time.sleep(1)
+   except:
+      traceback.print_exc()
+      
 load()
 
 
