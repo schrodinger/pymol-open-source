da9a73debe9f72fb983cfa9c96d2eaceae4f81b1
wdelano <wdelano@21428953-86e3-433d-9a34-2224224d0fc8>
2003-06-30 20:27:25 +0000

    *** empty log message ***

--- trunk/pymol/modules/pymol/xray.py
+++ trunk/pymol/modules/pymol/xray.py
@@ -12,34 +12,40 @@
 #-*
 #Z* -------------------------------------------------------------------
 
-import sglite
+try:
+   import sglite
 
-# xray.py 
-# This section contains python code for supporting
-# x-ray crystallography functions
+   # xray.py 
+   # This section contains python code for supporting
+   # x-ray crystallography functions
 
-def sg_sym_to_mat_list(sgsymbol):
-   try:
-      Symbols_Inp = sglite.SgSymbolLookup(sgsymbol)
-      if(Symbols_Inp):
-         HallSymbol = Symbols_Inp['Hall']
-         SgOps = sglite.SgOps(HallSymbol)
-         nLTr = SgOps.get_nLTr()
-         fInv = SgOps.get_fInv()
-         nSMx = SgOps.get_nSMx()
-         result = []
-         rb = float(sglite.SRBF)
-         tb = float(sglite.STBF)
-         for iLTr in xrange(nLTr):
-            for iInv in xrange(fInv):
-               for iSMx in xrange(nSMx):
-                  Mx = SgOps.getLISMx(iLTr, iInv, iSMx, +1)
-                  result.append([[ Mx[0]/rb, Mx[1]/rb, Mx[2]/rb, Mx[9 ]/tb],
-                                 [ Mx[3]/rb, Mx[4]/rb, Mx[5]/rb, Mx[10]/tb],
-                                 [ Mx[6]/rb, Mx[7]/rb, Mx[8]/rb, Mx[11]/tb],
-                                 [        0,        0,        0,         1]] )                  
-   except:
-      print " pmx: unrecognized space group symbol '"+sgsymbol+"'"
-      result = None
-   return result
+   def sg_sym_to_mat_list(sgsymbol):
+      try:
+         Symbols_Inp = sglite.SgSymbolLookup(sgsymbol)
+         if(Symbols_Inp):
+            HallSymbol = Symbols_Inp['Hall']
+            SgOps = sglite.SgOps(HallSymbol)
+            nLTr = SgOps.get_nLTr()
+            fInv = SgOps.get_fInv()
+            nSMx = SgOps.get_nSMx()
+            result = []
+            rb = float(sglite.SRBF)
+            tb = float(sglite.STBF)
+            for iLTr in xrange(nLTr):
+               for iInv in xrange(fInv):
+                  for iSMx in xrange(nSMx):
+                     Mx = SgOps.getLISMx(iLTr, iInv, iSMx, +1)
+                     result.append([[ Mx[0]/rb, Mx[1]/rb, Mx[2]/rb, Mx[9 ]/tb],
+                                    [ Mx[3]/rb, Mx[4]/rb, Mx[5]/rb, Mx[10]/tb],
+                                    [ Mx[6]/rb, Mx[7]/rb, Mx[8]/rb, Mx[11]/tb],
+                                    [        0,        0,        0,         1]] )                  
+      except:
+         print " pmx: unrecognized space group symbol '"+sgsymbol+"'"
+         result = None
+      return result
 
+except:
+   print "Error: unable to import xray module (no symmetry support)."
+   def sg_sym_to_mat_list(sgsymbol):
+      return None
+   
