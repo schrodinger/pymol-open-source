18e933bf1c98960b0d55cb2af23d46078ffcbad3
wdelano <wdelano@21428953-86e3-433d-9a34-2224224d0fc8>
2005-10-03 07:57:34 +0000

    *** empty log message ***

--- trunk/pymol/layer5/PyMOL.c
+++ trunk/pymol/layer5/PyMOL.c
@@ -1928,12 +1928,11 @@ void PyMOL_Start(CPyMOL *I)
 #endif
 
 #ifdef _MACPYMOL_XCODE
-   if(G->Option->force_stereo) {
-	   G->StereoCapable = 1;
-       SettingSetGlobal_b(G,cSetting_stereo_double_pump_mono, true);
-       SettingSetGlobal_i(G,cSetting_stereo_mode, 1); 
-	   SettingSetGlobal_i(G,cSetting_show_progress, 0); 
-	   }
+	SettingSetGlobal_b(G,cSetting_stereo_double_pump_mono, true);
+    if(G->Option->stereo_capable) {
+		SettingSetGlobal_i(G,cSetting_stereo_mode, 1); 
+	    SettingSetGlobal_i(G,cSetting_show_progress, 0); 
+       }
 #endif
 
   I->RedisplayFlag = true;
