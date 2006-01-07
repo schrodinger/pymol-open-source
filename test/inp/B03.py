00015ca69f4f4cb3b7a83a948160a873468985f3
wdelano <wdelano@21428953-86e3-433d-9a34-2224224d0fc8>
2006-01-07 09:56:45 +0000

    *** empty log message ***

--- test/inp/B03.py
+++ test/inp/B03.py
@@ -8,7 +8,7 @@ import threading
 import time
 from pymol import cmd
 import sys, os, os.path
-
+import traceback
 
 ent_dir = "pdb"
 
@@ -20,27 +20,29 @@ def load():
 #   while list[0]!="pdb/f8/pdb1f8u":
 #      list.pop(0)
    for file in list:
-      cmd.delete('pdb')
-      cmd.load(file,'pdb')
-      cmd.set_title('pdb',1,os.path.split(file)[-1])
-      cmd.rewind()
-      cmd.orient('pdb')
-      cmd.refresh()
-      cmd.as("ribbon")
-      cmd.refresh()
-      cmd.as("sticks")
-      cmd.refresh()
-      sys.__stderr__.write(".")
-      sys.__stderr__.flush()
-      n = cmd.count_states()
-      if n>1:
+      try:
+         cmd.delete('pdb')
+         cmd.load(file,'pdb')
+         cmd.set_title('pdb',1,os.path.split(file)[-1])
          cmd.rewind()
-                     
-         sys.__stderr__.write(file+"\n")
+         cmd.orient('pdb')
+         cmd.refresh()
+         cmd.as("ribbon")
+         cmd.refresh()
+         cmd.as("sticks")
+         cmd.refresh()
+         sys.__stderr__.write(".")
          sys.__stderr__.flush()
-         for a in range(1,n+1):
-            cmd.forward()
-            cmd.refresh()
+         n = cmd.count_states()
+         if n>1:
+            cmd.rewind()
+            sys.__stderr__.write(file+"\n")
+            sys.__stderr__.flush()
+            for a in range(1,n+1):
+               cmd.forward()
+               cmd.refresh()
+      except:
+         traceback.print_exc()
 cmd.feedback('disable','symmetry objectmolecule executive','everything')
 load()
 
