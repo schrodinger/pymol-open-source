84cd166b6c7d2e83842a87165c40270e98ae9e9e
wdelano <wdelano@21428953-86e3-433d-9a34-2224224d0fc8>
2006-02-17 14:17:32 +0000

    *** empty log message ***

--- test/inp/B02.py
+++ test/inp/B02.py
@@ -25,31 +25,35 @@ def load():
 #    while list[0]!="pdb/f8/pdb1f8u":
 #        list.pop(0)
     for file in list:
-        print file
-        cmd.delete('pdb')
-        cmd.load(file,'pdb')
-        cmd.orient('pdb')
-        cmd.color('red','ss h')
-        cmd.color('yellow','ss s')
-        cmd.hide()
-        if cmd.count_atoms()<15000:
-            cmd.show(rep[r],"all")
-        elif cmd.count_atoms()<50000:
-            cmd.show("cartoon","all")	
-        else:
-            cmd.show("lines","all")	
+        try:
+            print file                     
+            cmd.delete('pdb')              
+            cmd.load(file,'pdb')           
+            cmd.orient('pdb')              
+            cmd.color('red','ss h')        
+            cmd.color('yellow','ss s')     
+            cmd.hide()                     
+            if cmd.count_atoms()<15000:    
+                cmd.show(rep[r],"all")     
+            elif cmd.count_atoms()<50000:  
+                cmd.show("cartoon","all")  
+            else:                          
+                cmd.show("lines","all")	   
+                                           
+            r = r + 1                      
+            if r>=len(rep): r=0;           
+            sys.__stderr__.write(".")      
+            sys.__stderr__.flush()         
+            n = cmd.count_states()         
+            cmd.ray(160,120)               
+            cmd.dirty()                    
+            time.sleep(0.1)                
+            if n>1:                        
+                sys.__stderr__.write("\n") 
+                sys.__stderr__.flush()     
+        except:
+            traceback.print_exc()
 
-        r = r + 1
-        if r>=len(rep): r=0;
-        sys.__stderr__.write(".")
-        sys.__stderr__.flush()
-        n = cmd.count_states()
-        cmd.ray(160,120)
-	cmd.dirty()
-	time.sleep(0.1)
-        if n>1:
-            sys.__stderr__.write("\n")
-            sys.__stderr__.flush()
 try:
     load()
 except:
