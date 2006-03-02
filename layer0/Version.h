b897f56d16ac38b43f891ea93b8977e020d2ae56
wdelano <wdelano@21428953-86e3-433d-9a34-2224224d0fc8>
2006-03-02 02:15:57 +0000

    *** empty log message ***

--- layer0/Version.h
+++ layer0/Version.h
@@ -1,9 +1,9 @@
 #ifndef _PyMOL_VERSION
-#define _PyMOL_VERSION "0.99pre04"
+#define _PyMOL_VERSION "0.99pre05"
 #endif
 
 #ifndef _PyMOL_VERSION_double
-#define _PyMOL_VERSION_double 0.990301
+#define _PyMOL_VERSION_double 0.990401
 #endif
 
 /* for session file compatibility */
