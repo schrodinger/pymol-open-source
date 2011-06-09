34d73b81cfdbd67138dca04f78c6bc892ab7f99d
Jason Vertrees <1031738+inchoate@users.noreply.github.com>
2011-06-09 19:58:09 +0000

    fixed volume shaders

--- data/shaders/volume.fs
+++ data/shaders/volume.fs
@@ -1,12 +1,18 @@
 uniform sampler3D volumeTex;
 uniform sampler1D colorTex;
+
+uniform float fog_r;
+uniform float fog_g;
+uniform float fog_b;
+uniform float fog_enabled;
 varying float fog;
 
 void main()
 {
-  vec3 fog_color = vec3(0.0, 0.0, 0.0);
+  vec3 fog_color = vec3(fog_r, fog_g, fog_b);
   vec4 color = texture1D(colorTex, texture3D(volumeTex, gl_TexCoord[0].xyz).x);
   if (color.a == 0.0) discard;
-  gl_FragColor = vec4(vec3(mix(fog_color, color.rgb, fog)), color.a);
+  float cfog = mix(1.0, clamp(fog, 0.0, 1.0), fog_enabled);
+  gl_FragColor = vec4(vec3(mix(fog_color, color.rgb, cfog)), color.a);
 }
 
