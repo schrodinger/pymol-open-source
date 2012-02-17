b7cd55df55e229f919650d60c19523ca1a3c500f
blainebell1 <blainebell1@21428953-86e3-433d-9a34-2224224d0fc8>
2012-02-17 16:54:02 +0000

    updated version 1.5.0.1 to 1.5.0.2

--- setup.py
+++ setup.py
@@ -206,7 +206,7 @@ else: # linux or other unix
 
 distribution = setup ( # Distribution meta-data
     name      = "pymol",
-    version   = "1.5.0.1", # see layer0/Version.h for updated version
+    version   = "1.5.0.2", # see layer0/Version.h for updated version
     author    = "Schrodinger",
     url       = "http://pymol.org",
     contact   = "pymol-users@lists.sourceforge.net",
