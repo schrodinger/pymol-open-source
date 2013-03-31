22b08f8a21b64cea2fa4a9aa6082c039808259ed
Thomas Holder <thomas.holder@schrodinger.com>
2013-03-31 14:27:56 +0200

    PYMOL-1240: ANISOU vector rotation test

--- testing/jira/PYMOL-1240.py
+++ testing/jira/PYMOL-1240.py
@@ -7,7 +7,12 @@ from pymol import cmd, testing, stored
 
 v_pdbstr_anisou = (
     'ATOM      1  N   ARG A 197       5.287   1.830   7.079  1.00 30.85           N  \n'
-    'ANISOU    1  N   ARG A 197     3652   4226   3841   -164    -54    -45       N  \n'
+    'ANISOU    1  N   ARG A 197      652   4226   3841   -164    -54    -45       N  \n'
+    'END\n')
+
+v_pdbstr_rotated = (
+    'ATOM      1  N   ARG A 197      24.081   8.670   7.079  1.00 30.85           N  \n'
+    'ANISOU    1  N   ARG A 197     1839   3790   3091  -1033   1258    503       N  \n'
     'END\n')
 
 class TestPYMOL1240(testing.PyMOLTestCase):
@@ -16,3 +21,14 @@ class TestPYMOL1240(testing.PyMOLTestCase):
         cmd.read_pdbstr(v_pdbstr_anisou, 'm1')
         v = cmd.get_pdbstr()
         self.assertEqual(v, v_pdbstr_anisou, 'ANISOU records missing:\n' + v)
+
+        cmd.rotate('y', 30, object='m1')
+        cmd.translate([20, 0, 0], object='m1')
+        cmd.rotate('z', 20, object='m1')
+
+        v = cmd.get_pdbstr()
+        self.assertEqual(v, v_pdbstr_rotated, 'ANISOU not rotated in PDB string' + v)
+
+        v = cmd.get_model('m1').atom[0].u_aniso
+        self.assertArrayEqual(v, [0.183853, 0.378995, 0.309052, -0.103350, 0.125751, 0.050349],
+                1e-5, 'ANISOU not rotated in chempy model')
