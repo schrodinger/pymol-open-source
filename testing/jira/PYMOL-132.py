44954a87870f6578ef575d4f7d64a51faade26c8
Thomas Holder <thomas.holder@schrodinger.com>
2013-07-30 16:05:46 +0200

    PYMOL-132

---
+++ testing/jira/PYMOL-132.py
@@ -0,0 +1,26 @@
+'''
+duplicated CONECT records
+'''
+
+import unittest
+from collections import defaultdict
+from pymol import cmd, testing, stored
+
+class Test132(testing.PyMOLTestCase):
+
+    @testing.foreach(0, 1)
+    def test(self, nodup):
+        cmd.set('pdb_conect_nodup', nodup)
+        cmd.fragment('cyclopentadiene')
+        lines = cmd.get_pdbstr().splitlines()
+        bonds = defaultdict(int)
+        for line in lines:
+            if line.startswith('CONECT'):
+                indices = map(int, line[6:].split())
+                for i in indices[1:]:
+                    bonds[indices[0], i] += 1
+        counts = bonds.values()
+        if nodup:
+            self.assertTrue(all(v == 1 for v in counts))
+        else:
+            self.assertTrue(any(v > 1 for v in counts))
