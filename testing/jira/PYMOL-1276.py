120706587375dbf00efa969f821766f4ac4d1532
Thomas Holder <thomas.holder@schrodinger.com>
2013-08-01 15:55:57 +0200

    PYMOL-1276

---
+++ testing/jira/PYMOL-1276.py
@@ -0,0 +1,18 @@
+'''
+undo atom type change
+'''
+
+import unittest
+from pymol import cmd, testing, stored
+
+@testing.requires('incentive')
+class Test1276(testing.PyMOLTestCase):
+
+    def test(self):
+        cmd.fragment('gly', 'm1')
+        cmd.create('m2', 'm1')
+        cmd.edit('m1 & name N')
+        cmd.replace('I', 1, 1)
+        self.assertEqual(cmd.count_atoms('m1 in m2'), 5)
+        cmd.undo()
+        self.assertEqual(cmd.count_atoms('m1 in m2'), 7)
