b14c0d8105936472342fc422af647d404a004ad9
Thomas Holder <thomas.holder@schrodinger.com>
2013-08-16 09:38:24 +0200

    more preset tests

--- testing/api/preset.py
+++ testing/api/preset.py
@@ -40,3 +40,31 @@ class TestPreset(testing.PyMOLTestCase):
         self._assertPrettyReps()
         self.assertFalse(cmd.get_setting_boolean('cartoon_smooth_loops', '1oky'))
         self.assertFalse(cmd.get_setting_boolean('cartoon_fancy_helices', '1oky'))
+
+    def testLigands(self):
+        cmd.load(self.datafile('1oky.pdb.gz'))
+        preset.ligands('*')
+        self.assertEqual(cmd.count_atoms('rep cartoon'), 0)
+        self.assertGreater(cmd.count_atoms('rep ribbon'), 0)
+        self.assertEqual(cmd.count_atoms('rep sticks & organic'), cmd.count_atoms('organic'))
+        solvent_nb_count = cmd.count_atoms('rep nonbonded & solvent')
+        self.assertGreater(solvent_nb_count, 0)
+        self.assertLess(solvent_nb_count, cmd.count_atoms('solvent'))
+        self.assertGreater(cmd.count_atoms('rep lines & polymer'), 0)
+        self.assertEqual(cmd.count_atoms('(polymer be. 10 of hetatm) and rep lines'), 0)
+        self.assertTrue('1oky_pol_conts' in cmd.get_names('all'))
+
+    def testDefault(self):
+        cmd.load(self.datafile('1oky.pdb.gz'))
+        cmd.color("yellow", "1oky") # color index 6
+        preset.default('1oky')
+        self.assertEqual(cmd.count_atoms('rep cartoon'), 0)
+        self.assertEqual(cmd.count_atoms('rep ribbon'), 0)
+        self.assertEqual(cmd.count_atoms('rep sticks'), 0)
+        self.assertEqual(cmd.count_atoms('rep spheres'), 0)
+        self.assertEqual(cmd.count_atoms('rep lines'), cmd.count_atoms())
+        self.assertEqual(cmd.count_atoms('rep nonbonded & solvent'), cmd.count_atoms('solvent'))
+        # object color must be preserved for carbons
+        color_set = set()
+        cmd.iterate('elem C', 'color_set.add(color)', space=locals())
+        self.assertItemsEqual(color_set, [6])
