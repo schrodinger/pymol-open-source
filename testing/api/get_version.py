475e367ba1f8552102ecf36090c82b342e4db769
speleo3 <speleo3@users.sourceforge.net>
2013-10-18 13:34:19 -0400

    get_version

--- testing/api/get_version.py
+++ testing/api/get_version.py
@@ -7,7 +7,7 @@ from pymol import cmd, testing
 class TestVersion(testing.PyMOLTestCase):
 
     def test_version(self):
-        v = cmd.get_version()
+        v = cmd.get_version()[:3]
         # check that all elements of v are the same, need to remove "." and strip trailing 0's
         s = set(map(lambda s: str(s).replace('.', '').rstrip('0'), v))
         self.assertEqual(len(s), 1)
