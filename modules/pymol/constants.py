f0f1303617c29680e28be96b5ce4009400e43f71
Thomas Holder <1239490+speleo3@users.noreply.github.com>
2013-10-31 15:56:14 +0000

    fix get_setting_updates

--- modules/pymol/constants.py
+++ modules/pymol/constants.py
@@ -112,14 +112,20 @@ nt_hidden_path_re = re.compile(r"\$[\/\\]")
 quote_alpha_list_re = re.compile(
     r'''([\[\,]\s*)([a-zA-Z_][a-zA-Z0-9_\ ]*[a-zA-Z0-9_]*)(\s*[\,\]])''')
 
-def safe_list_eval(st):
-    return eval(sanitize_list_re.sub('',st)) 
-
 def safe_alpha_list_eval(st):
     st = sanitize_alpha_list_re.sub('',st)
     st = quote_alpha_list_re.sub(r'\1"\2"\3',st) # need to do this twice
     st = quote_alpha_list_re.sub(r'\1"\2"\3',st)
     return eval(sanitize_alpha_list_re.sub('',st)) 
+
+class SafeEvalNS(object):
+    def __getitem__(self, name):
+        return name
+
+def safe_eval(st):
+    return eval(st, {}, SafeEvalNS())
+
+safe_list_eval = safe_eval
 
 QuietException = parsing.QuietException
 
