24ea9213576ba26a17dc789503fc4438386d830f
Blaine Bell <blaine.bell@schrodinger.com>
2013-11-08 15:07:34 -0500

    fixed get_version test

--- testing/api/get_version.py
+++ testing/api/get_version.py
@@ -5,9 +5,18 @@ Testing template for PyMOL testing
 from pymol import cmd, testing
 
 class TestVersion(testing.PyMOLTestCase):
+    def _get_num(self, x):
+        retlist = []
+        for ele in x:
+            if ele.isdigit() or ele == '.':
+                retlist.append(ele)
+            else:
+                break
+        return ''.join(retlist)
 
     def test_version(self):
         v = cmd.get_version()[:3]
-        # check that all elements of v are the same, need to remove "." and strip trailing 0's
-        s = set(map(lambda s: str(s).replace('.', '').rstrip('0'), v))
+        # check that all elements of v are the same, need to strip number off beginning,
+        # remove "." and strip trailing 0's
+        s = set(map(lambda s: self._get_num(str(s)).replace('.', '').rstrip('0'), v))
         self.assertEqual(len(s), 1)
