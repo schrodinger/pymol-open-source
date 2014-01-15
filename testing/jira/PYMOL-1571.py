8ff529bfa0b973bfd3223b49f62a0d21fabc1750
Thomas Holder <speleo3@users.sf.net>
2014-01-15 18:57:22 -0500

    test for bg_image_filename embedding

---
+++ testing/jira/PYMOL-1571.py
@@ -0,0 +1,26 @@
+'''
+bg_image_filename in session files
+'''
+
+import shutil
+import os, unittest
+from pymol import cmd, testing
+
+@testing.requires('incentive')
+class Test1571(testing.PyMOLTestCase):
+
+    def test(self):
+        cmd.set('opaque_background')
+        with testing.mktemp('tmp.pse') as session_filename:
+            with testing.mktemp('bg.png') as bg_image_filename:
+                cmd.bg_color('red')
+                cmd.png(bg_image_filename)
+                cmd.bg_color('blue')
+                cmd.set('bg_image_filename', bg_image_filename)
+                cmd.save(session_filename)
+
+            cmd.load(session_filename)
+        x = cmd.get('bg_image_filename')
+        self.assertTrue(x.startswith('data:'))
+        self.assertImageHasColor('red', delta=4)
+
