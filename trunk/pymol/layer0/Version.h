87d61811605635a4c9500da954343b3a80f0780e
speleo3 <speleo3@21428953-86e3-433d-9a34-2224224d0fc8>
2014-01-20 23:35:30 +0000

    mutagenesis wizard: use PYMOL_DATA

--- trunk/pymol/layer0/Version.h
+++ trunk/pymol/layer0/Version.h
@@ -1,5 +1,5 @@
 #ifndef _PyMOL_VERSION
-#define _PyMOL_VERSION "1.7.0.0"
+#define _PyMOL_VERSION "1.7.0.1"
 #endif
 
 #ifndef _PYMOL_1_x
@@ -7,12 +7,12 @@
 #endif
 
 #ifndef _PyMOL_VERSION_double
-#define _PyMOL_VERSION_double 1.700
+#define _PyMOL_VERSION_double 1.701
 #endif
 
 
 /* for session file compatibility */
 
 #ifndef _PyMOL_VERSION_int
-#define _PyMOL_VERSION_int 1700
+#define _PyMOL_VERSION_int 1701
 #endif
