a81e9d4f89144c404719c2aa52132307e541351a
Thomas Holder <thomas.holder@schrodinger.com>
2014-03-09 17:03:35 -0400

    pdbloading: no_run_all

--- testing/performance/pdbloading.py
+++ testing/performance/pdbloading.py
@@ -4,14 +4,16 @@
 Stress testing for pdb loading
 '''
 
+import time
 from pymol import cmd, testing, stored
 
+@testing.requires('no_run_all')
 class StressPDBLoading(testing.PyMOLTestCase):
     @testing.foreach('1rx1.pdb', '1aon.pdb')
     def testPDBLoad(self, dfile):
+        tm = []
         for i in range(1, 100):
-            with self.timing():
-                cmd.load(self.datafile(dfile))
-        tm = map(lambda x: x[1], self.timing().test.timings)
-        self.timing().test.timings = []
+            start = time.time()
+            cmd.load(self.datafile(dfile))
+            tm.append(time.time() - start)
         print "min time('", dfile, "')=", min(tm)
