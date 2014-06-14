eaae0b474a2b24b3b10b8e510dd23cefef04dab8
Thomas Holder <thomas.holder@schrodinger.com>
2014-06-14 16:29:31 +0200

    cif loading tests

---
+++ testing/api/cif.py
@@ -0,0 +1,11 @@
+from pymol import cmd, testing, stored
+
+class TestCIF(testing.PyMOLTestCase):
+
+    def testPYMOL1737(self):
+        cmd.load(self.datafile('ice_IV.cif'))
+        self.assertTrue('ice_IV' in cmd.get_object_list())
+
+    def testPYMOL1533(self):
+        cmd.load(self.datafile('1v5a-3models.cif'))
+        self.assertEqual(cmd.count_states(), 3)
