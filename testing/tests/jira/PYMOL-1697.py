22c3cba5a70d349136f316634333507e1ffba240
Thomas Holder <thomas.holder@schrodinger.com>
2014-10-03 11:52:34 -0400

    test for PYMOL-2207

--- testing/tests/jira/PYMOL-1697.py
+++ testing/tests/jira/PYMOL-1697.py
@@ -1,19 +1,29 @@
-'''
-PYMOL-317
-can't select by atom type
-'''
-
 import os
 from pymol import cmd, testing, stored
 
 @testing.requires('incentive')
-class TestPYMOL1567(testing.PyMOLTestCase):
+class TestPYMOL1697(testing.PyMOLTestCase):
 
     @testing.foreach(1, 0)
+    @testing.requires_version('1.7.0')
     def testUndoAfterRemoveAtomOnDiscrete(self, discr):
+        cmd.set('suspend_undo', 0)
         cmd.load(self.datafile('ligs3d.sdf'), discrete=discr)
         natms = cmd.count_atoms()
         cmd.remove("index 1")
         cmd.undo()
         self.assertEqual(natms, cmd.count_atoms())
 
+    @testing.requires_version('1.7.3')
+    def testRegression2207(self):
+        '''
+        The initial fix for PYMOL-1697 crashed this example
+        (PYMOL-2207)
+        '''
+        cmd.load(self.datafile('1oky-frag.pdb'))
+        cmd.create('1oky-frag', 'resi 80-100', 1, 2)
+        cmd.remove('resi 85-95')
+        # keep ray efford low
+        cmd.viewport(50, 50)
+        # caused segfault
+        img = self.get_imagearray(ray=1)
