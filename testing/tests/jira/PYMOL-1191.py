452cd88273a6773df615c60fb111dd3cc01e15e1
Thomas Holder <thomas.holder@schrodinger.com>
2015-03-04 12:06:51 -0500

    fix MOE loading test

--- testing/tests/jira/PYMOL-1191.py
+++ testing/tests/jira/PYMOL-1191.py
@@ -6,11 +6,13 @@ MOE 2012 surfaces
 from pymol import cmd, testing, stored
 
 @testing.requires('incentive')
+@testing.requires_version('1.7.4')
 class TestPYMOL1191(testing.PyMOLTestCase):
 
     def testLoadMOE2012(self):
         cmd.load(self.datafile('benzene.moe.gz'))
 
         v = cmd.get_names()
-        self.assertTrue('benzene.system' in v)
+        self.assertTrue('benzene.system' not in v)
+        self.assertTrue('benzene.chain_1' in v)
         self.assertTrue('benzene.Quick_Surface_Ligand_' in v)
