eec75d2b08c0c0b0a06c966682da52f664e18a6c
Thomas Holder <thomas.holder@schrodinger.com>
2015-08-12 16:31:08 -0400

    CGO PSE test

---
+++ testing/tests/cgos/misc.py
@@ -0,0 +1,28 @@
+from pymol import cmd, testing, cgo
+import unittest
+
+class Test(testing.PyMOLTestCase):
+
+    def test_pse(self):
+        self.ambientOnly()
+
+        viewport = (50, 50)
+        cmd.viewport(*viewport)
+
+        objname = 'simpletri'
+        obj = []
+        obj.extend([
+            cgo.BEGIN, cgo.TRIANGLES,
+            cgo.COLOR, 1.0, 0., 0.,
+            cgo.VERTEX, 0.0, 0.0, 0.0,
+            cgo.VERTEX, 1.0, 0.0, 0.0,
+            cgo.VERTEX, 0.0, 1.0, 0.0,
+            cgo.END
+            ])
+        cmd.load_cgo(obj, objname, 1)
+
+        s = cmd.get_session()
+        cmd.set_session(s)
+
+        self.assertEqual([objname], cmd.get_names())
+        self.assertImageHasColor('red')
