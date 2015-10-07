f07032e909dbc6274ccca84d21a2867394823ac2
Thomas Holder <thomas.holder@schrodinger.com>
2015-10-07 18:14:04 -0400

    test for PYMOL-2589

---
+++ testing/tests/jira/PYMOL-2589.py
@@ -0,0 +1,29 @@
+'''
+some stick_ball caps missing
+'''
+
+import pymol
+from pymol import cmd, util, testing, stored
+
+@testing.requires('incentive')
+class Test2589(testing.PyMOLTestCase):
+
+    def test(self):
+        if not (pymol.invocation.options.no_gui or cmd.get_setting_int('use_shaders')):
+            self.skipTest('no ray or shaders')
+
+        self.ambientOnly()
+
+        cmd.viewport(350, 200)
+        cmd.fragment('ile')
+        cmd.remove('hydro')
+        cmd.show_as('sticks')
+        cmd.orient()
+
+        cmd.set('stick_ball', 0)
+        img1 = self.get_imagearray()
+
+        cmd.set('stick_ball', 1)
+        img2 = self.get_imagearray()
+
+        self.assertImageEqual(img1, img2, count=10)
