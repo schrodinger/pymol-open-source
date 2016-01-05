3d64d48191c4f6f6e945050935e883a831a60419
Thomas Holder <thomas.holder@schrodinger.com>
2016-01-05 15:13:24 -0500

    PYMOL-2688

---
+++ testing/tests/jira/PYMOL-2688.py
@@ -0,0 +1,11 @@
+'''
+PYMOL-2688 1.8.0 crashes with 1.5.0 session file
+'''
+
+from pymol import cmd, CmdException, testing, stored
+
+class TestPYMOL2688(testing.PyMOLTestCase):
+
+    def test(self):
+        cmd.load('PYMOL-2688.pse')
+        self.assertEqual(['myramp'], cmd.get_names())
