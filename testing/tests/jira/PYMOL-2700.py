578c44054c627df8572da8c9dba78da91ab59c1f
Thomas Holder <thomas.holder@schrodinger.com>
2016-01-17 15:46:26 -0500

    PYMOL-2700 PYMOL-2701

---
+++ testing/tests/jira/PYMOL-2700.py
@@ -0,0 +1,27 @@
+'''
+mol2 partial charge export
+'''
+
+from pymol import cmd, CmdException, testing, stored
+
+class TestPYMOL2700(testing.PyMOLTestCase):
+
+    @testing.requires_version('1.8.0.3') # 1.8.0.4
+    def testMol2PartialChargeExport(self):
+        cmd.fragment('gly')
+
+        charges_pre = []
+        cmd.iterate('*', 'charges_pre.append(partial_charge)', space=locals())
+
+        # fragment library has charges, but better check...
+        self.assertTrue(sum(abs(c) for c in charges_pre) > 0.0)
+
+        with testing.mktemp('.mol2') as filename:
+            cmd.save(filename)
+            cmd.delete('*')
+            cmd.load(filename)
+
+        charges_post = []
+        cmd.iterate('*', 'charges_post.append(partial_charge)', space=locals())
+
+        self.assertArrayEqual(charges_pre, charges_post, delta=1e-2)
---
+++ testing/tests/jira/PYMOL-2701.py
@@ -0,0 +1,20 @@
+'''
+mol2 formal charge assigment (import)
+'''
+
+from pymol import cmd, CmdException, testing, stored
+
+class TestPYMOL2701(testing.PyMOLTestCase):
+
+    @testing.requires_version('1.8.0.3') # 1.8.0.4
+    def testMol2PartialChargeExport(self):
+        cmd.fragment('lys')
+        cmd.fragment('lys')
+
+        with testing.mktemp('.mol2') as filename:
+            cmd.save(filename, state=0)
+            cmd.delete('*')
+            cmd.load(filename)
+
+        self.assertEqual(cmd.get_model('name NZ', state=1).atom[0].formal_charge, 1)
+        self.assertEqual(cmd.get_model('name NZ', state=2).atom[0].formal_charge, 1)
