fb6c82b359c527c330c76e6ec63a0e6b7b82bd4c
Thomas Holder <thomas.holder@schrodinger.com>
2016-11-21 16:48:14 -0500

    PYMOL-2832 ribbon coloring test

---
+++ testing/tests/jira/PYMOL-2832.py
@@ -0,0 +1,22 @@
+from pymol import cmd, testing
+
+@testing.requires_version('1.8.0')
+@testing.requires('incentive')
+class TestDiscreteRibbonColors(testing.PyMOLTestCase):
+
+    @testing.foreach(1, 0)
+    def test(self, ribbon_as_cylinders):
+        cmd.set('ribbon_as_cylinders', ribbon_as_cylinders)
+        cmd.set('ribbon_width', 8)
+        cmd.fab('AG')
+        cmd.color('red', 'resn ALA')
+        cmd.color('blue', 'resn GLY')
+        cmd.show_as('ribbon')
+        cmd.orient()
+
+        self.ambientOnly()
+        cmd.viewport(100, 100)
+        cmd.draw(100, 100)
+        img = self.get_imagearray()
+        self.assertImageHasColor('red', img)
+        self.assertImageHasColor('blue', img)
