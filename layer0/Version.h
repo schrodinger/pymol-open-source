463a47e689ccf75f59d4124afc0451dc8cea2bbc
Thomas Holder <1239490+speleo3@users.noreply.github.com>
2017-03-09 18:13:57 +0000

    1.8.6.0

--- layer0/Version.h
+++ layer0/Version.h
@@ -1,14 +1,14 @@
 #ifndef _PyMOL_VERSION
-#define _PyMOL_VERSION "1.8.5.1"
+#define _PyMOL_VERSION "1.8.6.0"
 #endif
 
 #ifndef _PyMOL_VERSION_double
-#define _PyMOL_VERSION_double 1.851
+#define _PyMOL_VERSION_double 1.860
 #endif
 
 
 /* for session file compatibility */
 
 #ifndef _PyMOL_VERSION_int
-#define _PyMOL_VERSION_int 1851
+#define _PyMOL_VERSION_int 1860
 #endif
