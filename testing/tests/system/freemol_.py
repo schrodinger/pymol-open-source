f16511f2e15a2707aecbc34e1287144d7e6dadde
Thomas Holder <thomas.holder@schrodinger.com>
2017-11-28 11:24:36 +0100

    disable freemol.apbs._psize_py validation

--- testing/tests/system/freemol_.py
+++ testing/tests/system/freemol_.py
@@ -11,6 +11,10 @@ class TestFreemol(testing.PyMOLTestCase):
         import freemol.apbs
         import freemol.mengine
         import freemol.mpeg_encode
+
+        # psize.py not used with PyMOL 2.0
+        freemol.apbs._psize_py = '<ignore>'
+
         self.assertTrue(freemol.apbs.validate())
         self.assertTrue(freemol.mengine.validate())
         self.assertTrue(freemol.mpeg_encode.validate())
