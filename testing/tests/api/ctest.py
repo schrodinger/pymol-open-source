5285d06ec72d059fbb6b4ac7687642969e995c69
Thomas Holder <thomas.holder@schrodinger.com>
2018-11-15 15:08:21 +0100

    run C tests

---
+++ testing/tests/api/ctest.py
@@ -0,0 +1,12 @@
+import pymol
+from pymol import cmd, testing
+
+@testing.requires_version('2.3')
+class TestCTest(testing.PyMOLTestCase):
+
+    def test2(self):
+        try:
+            status = pymol._cmd.test2()
+        except NotImplementedError:
+            self.skipTest("not compiled with --testing")
+        self.assertEqual(status, None)
