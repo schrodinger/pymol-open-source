3b01693828aff9f0a400ba84bf31be5c64d8fa4a
Thomas Holder <thomas.holder@schrodinger.com>
2019-01-26 15:32:39 +0100

    fix cmd.machine_get_clipboard

--- modules/pymol/xwin.py
+++ modules/pymol/xwin.py
@@ -4,19 +4,19 @@ import os
 
 def machine_get_clipboard():
     result = []
-    from popen2 import popen2
     wish_path = os.environ['PYMOL_PATH']+"/ext/bin/wish8.0" # dubious...
     if not os.path.exists(wish_path):
         wish_path = "/usr/bin/wish"
     if os.path.exists(wish_path):
-        pipe = popen2(wish_path)
-        pipe[1].write("puts [ selection get ]\n")
-        pipe[1].write("exit\n")
-        pipe[1].close()
-        while 1:
-            l=pipe[0].readline()
-            if not l: break
-            result.append(l)
+        import subprocess
+        p = subprocess.Popen([wish_path],
+                universal_newlines=True,
+                stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE)
+        p.stdin.write("puts [ selection get ]\n")
+        p.stdin.write("exit\n")
+        p.stdin.close()
+        result = p.stdout.readlines()
     return result
 
                       
