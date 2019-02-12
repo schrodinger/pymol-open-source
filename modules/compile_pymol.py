31c0ced8e2ef9589aadd62507245abbc9b7791b0
Thomas Holder <thomas.holder@schrodinger.com>
2019-02-12 17:20:34 +0100

    cleanup: remove obsolete setup files

--- modules/compile_pymol.py
+++
@@ -1,6 +0,0 @@
-from compileall import compile_dir
-
-compile_dir("modules",force=True)
-compile_dir("epymol/modules",force=True)
-#compile_dir("ipymol/modules",force=True)
-
--- modules/sitecustomize.py
+++
@@ -1,5 +0,0 @@
-# NOTE: This only works for module-based PyMOL builds.
-# Embedded versions of PyMOL call PyUnicode_SetDefaultEncoding at startup
-import sys
-if sys.version_info[0] < 3:
-    sys.setdefaultencoding("utf-8")
