1e7eca6dbb22c3d098c7a389265e5459d2cd1d2d
Thomas Holder <thomas.holder@schrodinger.com>
2019-05-16 09:05:06 +0200

    diagnostics_no_cmd

--- modules/pymol/diagnosing.py
+++ modules/pymol/diagnosing.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 def _unicode(s):
     # Return - if possible - unicode, or if the byte string can't be
@@ -25,12 +26,7 @@ ARGUMENTS
 
     filename = str: If given, write output to text file
     '''
-    import platform
-    import os
-    import re
-    import glob
     import time
-    import textwrap
     from pymol import invocation
     from pymol import cmd, CmdException
 
@@ -47,28 +43,14 @@ ARGUMENTS
         if version[4]:
             body += 'git sha: %s\n' % version[4]
 
-    try:
-        # pymol conda package version
-        condameta = glob.glob(os.path.join(sys.prefix, 'conda-meta',
-            'pymol-' + version[0] + '*.json'))
-        if condameta:
-            import json
-            d = json.load(open(condameta[0]))
-            body += 'conda build: %s %s\n' % (d.get('build'),
-                    d.get('schannel') or d.get('channel'))
-    except BaseException as e:
-        print(e)
+    body += diagnostics_conda(version[0])
 
     if not compact:
         body += '\nLicense Information:\n'
 
     body += 'Open-Source Build\n'
 
-    if not compact:
-        body += '\nOperating System:\n'
-    body += platform.platform() + '\n'
-    if platform.system() == 'Linux':
-        body += platform.version() + '\n'
+    body += diagnostics_platform()
 
     if not compact:
         body += '\nOpenGL Driver:\n'
@@ -77,21 +59,10 @@ ARGUMENTS
     body += str(renderer[1] or '(none)') + '\n'
     body += str(renderer[2] or '(none)') + '\n'
 
-    try:
-        from pymol.Qt import QtCore
-        body += '%s %s (Qt %s)\n' % (
-                QtCore.__name__.split('.')[0],
-                QtCore.PYQT_VERSION_STR,
-                QtCore.QT_VERSION_STR)
-    except Exception as e:
-        body += '(%s)\n' % (e,)
+    body += diagnostics_qt()
 
     if not compact:
-        body += '\nPython:\n'
-        body += '%s\n' % sys.version
-        body += 'prefix=%s\n' % _unicode(sys.prefix)
-        body += 'executable=%s\n' % _unicode(sys.executable)
-        body += u'filesystemencoding={}\n'.format(sys.getfilesystemencoding())
+        body += diagnostics_python()
 
         body += '\nStartup Scripts:\n'
         pymolrc = invocation.get_user_config()
@@ -101,13 +72,7 @@ ARGUMENTS
         else:
             body += '(no pymolrc file found)\n'
 
-        body += '\nQt, Python and PyMOL Environment Variables:\n'
-        for key in sorted(os.environ):
-            if re.search(r'^PY|QT|^LANG', key) and not (key == 'PYMOL_SCRIPTS'):
-                body += u'%s=%s\n' % (key, _unicode(os.environ[key]))
-
-        body += '\nPATH:\n'
-        body += textwrap.fill(_unicode(os.getenv('PATH', '')), 78) + '\n'
+        body += diagnostics_env_vars()
 
         body += '\nDiagnostics collected on %s %s\n' % (time.ctime(), TZ)
 
@@ -122,4 +87,88 @@ ARGUMENTS
     elif not quiet:
         print(body.rstrip())
 
+    return body
+
+
+def diagnostics_no_cmd():
+    '''
+    System level diagnostics data which doesn't depend on pymol._cmd
+    '''
+    body = ''
+    body += diagnostics_conda()
+    body += diagnostics_platform()
+    body += diagnostics_qt()
+    body += diagnostics_python()
+    body += diagnostics_env_vars()
+    return body
+
+
+def diagnostics_conda(versionselector=''):
+    import glob
+    body = ''
+    try:
+        # pymol conda package version
+        condameta = glob.glob(os.path.join(sys.prefix, 'conda-meta',
+            'pymol-' + versionselector + '*.json'))
+        for jsonfile in condameta:
+            import json
+            d = json.load(open(jsonfile))
+            if not versionselector:
+                body += os.path.basename(jsonfile) + '\n'
+            body += u'conda build: {} {}\n'.format(d.get('build'),
+                    d.get('schannel') or d.get('channel'))
+    except BaseException as e:
+        print(e)
+    return body
+
+
+def diagnostics_platform(compact=False):
+    import platform
+    body = u''
+
+    if not compact:
+        body += '\nOperating System:\n'
+
+    body += platform.platform() + '\n'
+
+    if platform.system() == 'Linux':
+        body += platform.version() + '\n'
+
+    return body
+
+
+def diagnostics_qt():
+    try:
+        from pymol.Qt import QtCore
+        return u'{} {} (Qt {})\n'.format(
+                QtCore.__name__.split('.')[0],
+                QtCore.PYQT_VERSION_STR,
+                QtCore.QT_VERSION_STR)
+    except Exception as e:
+        return u'({})\n'.format(e)
+
+
+def diagnostics_python():
+    tmpl = (u'\nPython:\n'
+            u'{v}\n'
+            u'prefix={p}\n'
+            u'executable={exe}\n'
+            u'filesystemencoding={enc}\n')
+    return tmpl.format(v=sys.version,
+                       p=_unicode(sys.prefix),
+                       exe=_unicode(sys.executable),
+                       enc=sys.getfilesystemencoding())
+
+
+def diagnostics_env_vars():
+    import re
+    import textwrap
+
+    body = '\nQt, Python and PyMOL Environment Variables:\n'
+    for key in sorted(os.environ):
+        if re.search(r'^PY|QT|^LANG', key) and not (key == 'PYMOL_SCRIPTS'):
+            body += u'%s=%s\n' % (key, _unicode(os.environ[key]))
+
+    body += '\nPATH:\n'
+    body += textwrap.fill(_unicode(os.getenv('PATH', '')), 78) + '\n'
     return body
