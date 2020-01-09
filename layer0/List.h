66628364733e3353cffc2b758d714da5870f07f1
Jarrett Johnson <jarrett.johnson@schrodinger.com>
2020-01-09 11:14:00 -0500

    pymol::ListAdapter - Range Adaptor for linked lists

---
+++ layer0/List.h
@@ -0,0 +1,105 @@
+#include <cassert>
+#include <iostream>
+#include <iterator>
+
+namespace pymol
+{
+
+/**
+ * Non-owning view into PyMOL's singly linked list.
+ */
+
+template <typename T> class ListAdapter
+{
+  /**
+   * Iterator for pymol::ListAdapter
+   * Not really  aiming to satisfy any of the standard
+   * named requirements for iterators, but to only implement
+   * ones that will likely be used.
+   * Uses IsConst pattern to avoid having to define non-const and const iterator
+   * versions
+   */
+
+  template <bool IsConst> class ListAdapterIterator
+  {
+  public:
+    using iterator_category = std::forward_iterator_tag;
+    using difference_type = std::ptrdiff_t;
+    using value_type = typename std::conditional<IsConst, const T, T>::type;
+    using pointer = value_type*;
+    using reference = value_type&;
+
+    explicit ListAdapterIterator(pointer ptr)
+        : m_ptr(ptr)
+    {
+    }
+
+    /**
+     * Creates a const_iterator from iterator but not the other way around
+     * @param other non const iterator
+     */
+    template <bool InnerIsConst = IsConst,
+        typename = typename std::enable_if<InnerIsConst>>
+    ListAdapterIterator(const ListAdapterIterator<false>& other)
+        : m_ptr(other.m_ptr)
+    {
+    }
+
+    reference operator*() { return *m_ptr; }
+
+    pointer operator->() { return m_ptr; }
+
+    ListAdapterIterator& operator++()
+    {
+      assert(m_ptr);
+      m_ptr = m_ptr->next;
+      return *this;
+    }
+
+    ListAdapterIterator operator++(int)
+    {
+      ListAdapterIterator tmp(*this);
+      operator++();
+      return tmp;
+    }
+
+    bool operator==(const ListAdapterIterator& iter) const
+    {
+      return m_ptr == iter.m_ptr;
+    }
+
+    bool operator!=(const ListAdapterIterator& iter) const
+    {
+      return !(m_ptr == iter.m_ptr);
+    }
+
+    friend class ListAdapterIterator<true>;
+
+  private:
+    pointer m_ptr;
+  };
+
+public:
+  using type = T;
+  using iterator = ListAdapterIterator<false>;
+  using const_iterator = ListAdapterIterator<true>;
+  explicit ListAdapter(T* list)
+      : m_list(list)
+  {
+  }
+  iterator begin() noexcept { return iterator(m_list); }
+  iterator end() noexcept { return iterator(nullptr); }
+  const_iterator begin() const noexcept { return const_iterator(m_list); }
+  const_iterator end() const noexcept { return const_iterator(nullptr); }
+  const_iterator cbegin() const noexcept { return const_iterator(m_list); }
+  const_iterator cend() const noexcept { return const_iterator(nullptr); }
+
+private:
+  type* m_list;
+};
+
+template <typename T> ListAdapter<T> make_list_adapter(T *list) {
+  return ListAdapter<T>(list);
+}
+
+} // namespace pymol
---
+++ layerCTest/Test_List.cpp
@@ -0,0 +1,205 @@
+#include "List.h"
+#include "ListMacros.h"
+#include "Test.h"
+
+#include <numeric>
+
+struct Foo {
+  int someInt;
+  Foo* next;
+};
+
+/**
+ * Mimics ListElemCalloc without PyMOLGlobals
+ * @return a zero-initialized instance on the free store
+ */
+template <typename T> static T* ListElemCallocNoG()
+{
+  static_assert(std::is_trivial<T>::value, "");
+  return pymol::calloc<T>(1);
+}
+
+TEST_CASE("Legacy List", "[List]")
+{
+  Foo* myList;
+  ListInit(myList);
+  for (int i = 0; i < 4; ++i) {
+    auto bar = ListElemCallocNoG<Foo>();
+    bar->someInt = i;
+    ListAppend(myList, bar, next, Foo);
+  }
+
+  Foo* fooPtr = nullptr;
+  int count = 0;
+  while (ListIterate(myList, fooPtr, next)) {
+    count += fooPtr->someInt;
+  }
+  REQUIRE(count == 6);
+  ListFree(myList, next, Foo);
+}
+
+
+TEST_CASE("pymol::ListAdapter type checks", "[List]")
+{
+  Foo* myList;
+  ListInit(myList);
+  auto myAdapter = pymol::make_list_adapter(myList);
+  static_assert(
+      std::is_same<decltype(myAdapter.begin())::value_type, Foo>::value,
+      "auto begin wrong type");
+  static_assert(
+      std::is_same<decltype(myAdapter.cbegin())::value_type, const Foo>::value,
+      "auto cbegin wrong type");
+  static_assert(std::is_same<decltype(myAdapter.end())::value_type, Foo>::value,
+      "auto end wrong type");
+  static_assert(
+      std::is_same<decltype(myAdapter.cend())::value_type, const Foo>::value,
+      "auto cend wrong type");
+  ListFree(myList, next, Foo);
+}
+
+TEST_CASE("pymol::ListAdapter For pre", "[List]")
+{
+  Foo* myList;
+  ListInit(myList);
+  for (int i = 0; i < 4; ++i) {
+    auto bar = ListElemCallocNoG<Foo>();
+    bar->someInt = i;
+    ListAppend(myList, bar, next, Foo);
+  }
+
+  auto myAdapter = pymol::make_list_adapter(myList);
+
+  int count = 0;
+  for (auto it = myAdapter.begin(); it != myAdapter.end(); ++it) {
+    count += it->someInt;
+  }
+  REQUIRE(count == 6);
+  ListFree(myList, next, Foo);
+}
+
+TEST_CASE("pymol::ListAdapter For post", "[List]")
+{
+  Foo* myList;
+  ListInit(myList);
+  for (int i = 0; i < 4; ++i) {
+    auto bar = ListElemCallocNoG<Foo>();
+    bar->someInt = i;
+    ListAppend(myList, bar, next, Foo);
+  }
+
+  auto myAdapter = pymol::make_list_adapter(myList);
+
+  int count = 0;
+  for (auto it = myAdapter.begin(); it != myAdapter.end(); it++) {
+    count += it->someInt;
+  }
+  REQUIRE(count == 6);
+  ListFree(myList, next, Foo);
+}
+
+TEST_CASE("pymol::ListAdapter For cbegin", "[List]")
+{
+  Foo* myList;
+  ListInit(myList);
+  for (int i = 0; i < 4; ++i) {
+    auto bar = ListElemCallocNoG<Foo>();
+    bar->someInt = i;
+    ListAppend(myList, bar, next, Foo);
+  }
+
+  auto myAdapter = pymol::make_list_adapter(myList);
+
+  int count = 0;
+  for (auto it = myAdapter.cbegin(); it != myAdapter.cend(); it++) {
+    count += it->someInt;
+  }
+  REQUIRE(count == 6);
+
+  ListFree(myList, next, Foo);
+}
+
+TEST_CASE("pymol::ListAdapter For const_iterator", "[List]")
+{
+  Foo* myList;
+  ListInit(myList);
+  for (int i = 0; i < 4; ++i) {
+    auto bar = ListElemCallocNoG<Foo>();
+    bar->someInt = i;
+    ListAppend(myList, bar, next, Foo);
+  }
+
+  auto myAdapter = pymol::make_list_adapter(myList);
+
+  int count = 0;
+  for (pymol::ListAdapter<Foo>::const_iterator it = myAdapter.begin();
+       it != myAdapter.end(); it++) {
+    count += it->someInt;
+  }
+  REQUIRE(count == 6);
+  ListFree(myList, next, Foo);
+}
+
+TEST_CASE("pymol::ListAdapter Ranged For", "[List]")
+{
+  Foo* myList;
+  ListInit(myList);
+  for (int i = 0; i < 4; ++i) {
+    auto bar = ListElemCallocNoG<Foo>();
+    bar->someInt = i;
+    ListAppend(myList, bar, next, Foo);
+  }
+
+  auto myAdapter = pymol::make_list_adapter(myList);
+
+  int count = 0;
+  for (const auto& foo : pymol::ListAdapter<Foo>(myList)) {
+    count += foo.someInt;
+  }
+  REQUIRE(count == 6);
+  ListFree(myList, next, Foo);
+}
+
+TEST_CASE("pymol::ListAdapter Std Algorithm InputIterator", "[List]")
+{
+  Foo* myList;
+  ListInit(myList);
+  for (int i = 0; i < 4; ++i) {
+    auto bar = ListElemCallocNoG<Foo>();
+    bar->someInt = i;
+    ListAppend(myList, bar, next, Foo);
+  }
+
+  auto myAdapter = pymol::make_list_adapter(myList);
+
+  int count = std::accumulate(myAdapter.begin(), myAdapter.end(), 0,
+      [](int sum, const Foo& b) { return sum + b.someInt; });
+  REQUIRE(count == 6);
+  ListFree(myList, next, Foo);
+}
+
+TEST_CASE("pymol::ListAdapter Std Algorithm OutputIterator", "[List]")
+{
+  Foo* myList;
+  ListInit(myList);
+  for (int i = 0; i < 4; ++i) {
+    auto bar = ListElemCallocNoG<Foo>();
+    bar->someInt = i;
+    ListAppend(myList, bar, next, Foo);
+  }
+
+  auto myAdapter = pymol::make_list_adapter(myList);
+
+  std::transform(
+      myAdapter.begin(), myAdapter.end(), myAdapter.begin(), [](Foo& foo) {
+        foo.someInt += 1;
+        return foo;
+      });
+
+  int count = std::accumulate(myAdapter.begin(), myAdapter.end(), 0,
+      [](int sum, const Foo& b) { return sum + b.someInt; });
+  REQUIRE(count == 10);
+  ListFree(myList, next, Foo);
+}
+
+// vi:sw=2:expandtab
