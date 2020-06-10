5d7fe693d9955cd6257f586af1f06583244ab318
Thomas Holder <thomas.holder@schrodinger.com>
2020-06-10 19:03:52 +0200

    mtz test

--- testing/tests/api/headering.py
+++ testing/tests/api/headering.py
@@ -17,3 +17,32 @@ class TestHeadering(testing.PyMOLTestCase):
             header = headering.MTZHeader(filename)
             self.assertTrue(isinstance(header, headering.MTZHeader))
             self.assertTrue(header.datasets)
+
+    def test2(self):
+        filename = self.datafile('4rwb.mtz')
+        header = headering.MTZHeader(filename)
+
+        self.assertTrue(isinstance(header, headering.MTZHeader))
+
+        ds0 = header.datasets['0']
+        self.assertEqual(ds0['name'], 'HKL_base')
+        self.assertEqual(ds0['project'], 'HKL_base')
+        self.assertAlmostEqual(float(ds0['alpha']), 90.0)
+        self.assertAlmostEqual(float(ds0['beta']), 95.677)
+        self.assertAlmostEqual(float(ds0['gamma']), 90.0)
+
+        ds1 = header.datasets['1']
+        self.assertEqual(ds1['name'], 'data_1')
+        self.assertEqual(ds1['project'], 'sf_convert')
+        self.assertAlmostEqual(float(ds1['x']), 40.93)
+        self.assertAlmostEqual(float(ds1['y']), 41.23)
+        self.assertAlmostEqual(float(ds1['z']), 27.93)
+
+        self.assertEqual(header.getColumns(), [
+            'HKL_base/HKL_base/H', 'HKL_base/HKL_base/K',
+            'HKL_base/HKL_base/L', 'cryst_1/data_1/FREE', 'cryst_1/data_1/FP',
+            'cryst_1/data_1/SIGFP', 'cryst_1/data_1/FC', 'cryst_1/data_1/PHIC',
+            'cryst_1/data_1/FOM'
+        ])
+
+        self.assertEqual(header.getColumnsOfType("W"), ['cryst_1/data_1/FOM'])
