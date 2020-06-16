6562e3278ed3db9ef522faa92e8e0a257c3ad872
Jarrett Johnson <jarrett.johnson@schrodinger.com>
2020-06-15 23:54:12 -0400

    undo clean test

--- testing/tests/api/computing.py
+++ testing/tests/api/computing.py
@@ -18,3 +18,22 @@ class TestComputing(testing.PyMOLTestCase):
         # energy for HIS = 32.73887
         self.assertTrue(30.0 < energy < 35.0)
 
+    @testing.requires("multi_undo")
+    def testUndoClean(self):
+        cmd.fragment('his')
+        xyz1 = cmd.get_model('his').get_coord_list()
+        energy = cmd.clean('his')
+        xyz2 = cmd.get_model('his').get_coord_list()
+        self.assertEqual(len(xyz1), len(xyz2))
+
+        # coords have changed
+        self.assertNotEqual(xyz1, xyz2)
+
+        cmd.undo2()
+        xyz2 = cmd.get_model('his').get_coord_list()
+        self.assertEqual(xyz1, xyz2)
+
+        cmd.redo2()
+        xyz2 = cmd.get_model('his').get_coord_list()
+        self.assertNotEqual(xyz1, xyz2)
+
