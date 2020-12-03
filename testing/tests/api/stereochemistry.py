75ded4a164c226629db38a64862d374934ee413e
Thomas Holder <thomas.holder@schrodinger.com>
2020-12-03 15:55:18 +0100

    test assign_stereo

---
+++ testing/tests/api/stereochemistry.py
@@ -0,0 +1,31 @@
+import os
+from pymol import movie, cmd, testing, stored
+
+@testing.requires('incentive')
+class TestStereochemistry(testing.PyMOLTestCase):
+
+    def test_assign_stereo_schrodinger(self):
+        if not os.getenv("SCHRODINGER"):
+            self.skipTest("SCHRODINGER not set")
+
+        self.assertEqual(cmd.count_atoms("stereo R"), 0)
+
+        cmd.load(self.datafile("1ehz-5.pdb"), "m1")
+        cmd.assign_stereo(method="schrodinger")
+
+        self.assertEqual(cmd.count_atoms("stereo R"), 15)
+        self.assertEqual(cmd.count_atoms("stereo S"), 5)
+
+    def test_assign_stereo_rdkit(self):
+        try:
+            import rdkit
+        except ImportError:
+            self.skipTest("rdkit not available")
+
+        self.assertEqual(cmd.count_atoms("stereo R"), 0)
+
+        cmd.load(self.datafile("1ehz-5.pdb"), "m1")
+        cmd.assign_stereo(method="rdkit")
+
+        self.assertEqual(cmd.count_atoms("stereo R & not elem P"), 15)
+        self.assertEqual(cmd.count_atoms("stereo S"), 5)
